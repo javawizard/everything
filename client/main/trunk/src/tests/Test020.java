4bf7eeff981f58d10d1ff915a571fc0489d35f16
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-07-03 02:18:32 +0000

    Added a small program for demoing the wizard library, and added some more documentation on the server protocol.
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@109 124d5c61-e844-0410-bd6f-bad94b02dd72

---
+++ client/main/trunk/src/tests/Test020.java
@@ -0,0 +1,97 @@
+package tests;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.plaf.basic.BasicBorders;
+
+import org.awl.DefaultWizardPageDescriptor;
+import org.awl.NavigationAuthorization;
+import org.awl.Wizard;
+import org.awl.WizardConstants;
+
+public class Test020
+{
+    
+    /**
+     * @param args
+     * @throws UnsupportedLookAndFeelException
+     * @throws IllegalAccessException
+     * @throws InstantiationException
+     * @throws ClassNotFoundException
+     */
+    public static void main(String[] args)
+        throws ClassNotFoundException,
+        InstantiationException, IllegalAccessException,
+        UnsupportedLookAndFeelException
+    {
+        Wizard wizard = new Wizard((JFrame) null);
+        DefaultWizardPageDescriptor p1 = new DefaultWizardPageDescriptor(
+            "title1");
+        p1.setDescription("this is page 1");
+        p1
+            .setPreviousDescriptorId(WizardConstants.STARTING_DESCRIPTOR_ID);
+        p1.setNextDescriptorId("2");
+        p1.setComponent(new JLabel("page 1 contents"));
+        final DefaultWizardPageDescriptor p2 = new DefaultWizardPageDescriptor(
+            "title1")
+        {
+            
+            @Override
+            public void displayingPanel(Wizard wizard)
+            {
+                // TODO Auto-generated method stub
+                System.out.println("displaying");
+                super.displayingPanel(wizard);
+            }
+            
+        };
+        
+        p2.setDescription("this is page 2");
+        p2.setPreviousDescriptorId("1");
+        p2
+            .setCancelAuthorization(NavigationAuthorization.FORBIDDEN);
+        p2
+            .setNextDescriptorId(WizardConstants.TERMINAL_DESCRIPTOR_ID);
+        JPanel p2panel = new JPanel();
+        JButton b = new JButton(
+            "click me to allow to continue");
+        p2panel.add(b);
+        p2
+            .setFinishAuthorization(NavigationAuthorization.FORBIDDEN);
+        b.addActionListener(new ActionListener()
+        {
+            
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                p2
+                    .setFinishAuthorization(NavigationAuthorization.ALLOWED);
+            }
+        });
+        p2.setComponent(p2panel);
+        wizard.registerWizardPanel("1", p1);
+        wizard.registerWizardPanel("2", p2);
+        wizard.setSize(400, 300);
+        wizard.getNextButton().setText(" Next ");
+        wizard.getBackButton().setText(" Back ");
+        wizard.getFinishButton().setText(" Finish ");
+        wizard.getCancelButton().setText(" Cancel ");
+        wizard.getNextButton().setBorder(
+            BasicBorders.getButtonBorder());
+        wizard.getBackButton().setBorder(
+            BasicBorders.getButtonBorder());
+        wizard.getFinishButton().setBorder(
+            BasicBorders.getButtonBorder());
+        wizard.getCancelButton().setBorder(
+            BasicBorders.getButtonBorder());
+        wizard.show();
+    }
+    
+}
