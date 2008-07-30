fd17aafda0cef74cc31c41c0ad201e5e0c7dc312
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-07-30 16:34:55 +0000

    Added code to the example class so that it shows a task with four subtasks in it
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@172 124d5c61-e844-0410-bd6f-bad94b02dd72

--- client/main/trunk/src/tests/Test023.java
+++ client/main/trunk/src/tests/Test023.java
@@ -22,9 +22,11 @@ public class Test023
         // A class for showing a ProgressPane with some items in it.
         JFrame frame = new JFrame();
         ProgressPane progress = new ProgressPane();
+        ProgressPane subprogress = new ProgressPane();
         JPanel panel = new JPanel();
         panel.setLayout(new BorderLayout());
         DefaultProgressItem[] items = new DefaultProgressItem[15];
+        DefaultProgressItem[] subitems = new DefaultProgressItem[4];
         frame.getContentPane().add(
             new JLabel("<html><b>Some items to do:</b>"),
             BorderLayout.NORTH);
@@ -37,22 +39,34 @@ public class Test023
         Thread.sleep(100);
         for (int i = 0; i < items.length; i++)
         {
-            items[i] = new DefaultProgressItem(
-                "Test item "
-                    + (i + 1)
-                    + (i == 4 ? " (This item will fail)"
-                        : ""),
-                "This is a description about item " + (i+1),
-                (i == 7 ? new JLabel(
-                    "These are the details") : null));
+            items[i] = new DefaultProgressItem("Test item "
+                + (i + 1)
+                + (i == 4 ? " (This item will fail)" : ""),
+                "This is a description about item "
+                    + (i + 1),
+                (i == 7 ? subprogress : null));
             if (i == 0)
                 items[i]
                     .setStatus(ProgressItem.Status.ACTIVE);
             progress.addItem(items[i]);
         }
+        for (int i = 0; i < subitems.length; i++)
+        {
+            subitems[i] = new DefaultProgressItem(
+                "Test subitem " + (i + 1),
+                "This is one of the subitems.", null);
+            subprogress.addItem(subitems[i]);
+        }
+        subprogress.invalidate();
+        progress.invalidate();
+        subprogress.validate();
+        progress.validate();
+        subprogress.repaint();
+        progress.repaint();
         for (int i = 0; i < items.length; i++)
         {
-            Thread.sleep(700);
+            if (i != 7)
+                Thread.sleep(700);
             if (i != 4)
                 items[i]
                     .setStatus(ProgressItem.Status.SUCCESSFUL);
@@ -62,6 +76,20 @@ public class Test023
             if ((i + 1) != items.length)
                 items[i + 1]
                     .setStatus(ProgressItem.Status.ACTIVE);
+            if (i == 6)
+            {
+                subitems[0]
+                    .setStatus(ProgressItem.Status.ACTIVE);
+                for (int s = 0; s < subitems.length; s++)
+                {
+                    Thread.sleep(700);
+                    subitems[s]
+                        .setStatus(ProgressItem.Status.SUCCESSFUL);
+                    if ((s + 1) != subitems.length)
+                        subitems[s + 1]
+                            .setStatus(ProgressItem.Status.ACTIVE);
+                }
+            }
             progress.refresh();
         }
     }
