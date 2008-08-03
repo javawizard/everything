8c5127656706f98047f049db775ea5a14526f942
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-08-03 07:39:00 +0000

    Added getters and setters for the Contact class
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@197 124d5c61-e844-0410-bd6f-bad94b02dd72

--- client/main/trunk/src/net/sf/opengroove/client/Contact.java
+++ client/main/trunk/src/net/sf/opengroove/client/Contact.java
@@ -18,4 +18,104 @@ public class Contact implements Serializable
     private BigInteger rsaSigMod;
     private boolean isUserContact;
     private boolean isUserVerified;
+    
+    public String getRealm()
+    {
+        return realm;
+    }
+    
+    public String getUsername()
+    {
+        return username;
+    }
+    
+    public String getRealName()
+    {
+        return realName;
+    }
+    
+    public String getLocalName()
+    {
+        return localName;
+    }
+    
+    public BigInteger getRsaEncPub()
+    {
+        return rsaEncPub;
+    }
+    
+    public BigInteger getRasEncMod()
+    {
+        return rasEncMod;
+    }
+    
+    public BigInteger getRsaSigPub()
+    {
+        return rsaSigPub;
+    }
+    
+    public BigInteger getRsaSigMod()
+    {
+        return rsaSigMod;
+    }
+    
+    public boolean isUserContact()
+    {
+        return isUserContact;
+    }
+    
+    public boolean isUserVerified()
+    {
+        return isUserVerified;
+    }
+    
+    public void setRealm(String realm)
+    {
+        this.realm = realm;
+    }
+    
+    public void setUsername(String username)
+    {
+        this.username = username;
+    }
+    
+    public void setRealName(String realName)
+    {
+        this.realName = realName;
+    }
+    
+    public void setLocalName(String localName)
+    {
+        this.localName = localName;
+    }
+    
+    public void setRsaEncPub(BigInteger rsaEncPub)
+    {
+        this.rsaEncPub = rsaEncPub;
+    }
+    
+    public void setRasEncMod(BigInteger rasEncMod)
+    {
+        this.rasEncMod = rasEncMod;
+    }
+    
+    public void setRsaSigPub(BigInteger rsaSigPub)
+    {
+        this.rsaSigPub = rsaSigPub;
+    }
+    
+    public void setRsaSigMod(BigInteger rsaSigMod)
+    {
+        this.rsaSigMod = rsaSigMod;
+    }
+    
+    public void setUserContact(boolean isUserContact)
+    {
+        this.isUserContact = isUserContact;
+    }
+    
+    public void setUserVerified(boolean isUserVerified)
+    {
+        this.isUserVerified = isUserVerified;
+    }
 }
