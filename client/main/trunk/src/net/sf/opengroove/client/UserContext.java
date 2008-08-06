a64631afed4b9923ae5a92e857630642ca47a264
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-08-06 15:43:53 +0000

    Added the UserContext and UserIds classes. UserIds is a class that will contain utilities related to user ids, such as extracting the realm portion or relativizing a userid (which changes it to a username if the user is of the same realm server as the user calling it, which is passed in via a UserContext class), and the UserContext class contains information about a particular user that's logged in, such as their userid (which can be used to obtain their Storage object), their launchbar window, etc.
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@210 124d5c61-e844-0410-bd6f-bad94b02dd72

---
+++ client/main/trunk/src/net/sf/opengroove/client/UserContext.java
@@ -0,0 +1,10 @@
+package net.sf.opengroove.client;
+/**
+ * A user context object is created for each user that logs in, and is passed down through 
+ * @author Alexander Boyd
+ *
+ */
+public class UserContext
+{
+    
+}
---
+++ client/main/trunk/src/net/sf/opengroove/client/UserIds.java
@@ -0,0 +1,6 @@
+package net.sf.opengroove.client;
+
+public class UserIds
+{
+    
+}
