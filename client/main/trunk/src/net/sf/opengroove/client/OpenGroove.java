8db051af7352bf2092e4400e61bab76adfd953d6
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-08-06 04:47:37 +0000

    Working a bit on the OpenGroove class, and added a class (Test024) for printing out the classname of a JButton's default border (which turned out to be javax.swing.plaf.BorderUIResource$CompoundBorderUIResource)
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@206 124d5c61-e844-0410-bd6f-bad94b02dd72

--- client/main/trunk/src/net/sf/opengroove/client/OpenGroove.java
+++ client/main/trunk/src/net/sf/opengroove/client/OpenGroove.java
@@ -3530,9 +3530,44 @@ public class OpenGroove
         src.delete();
     }
     
+    /**
+     * Converts the username and realm specified to a userid. Userids take the
+     * format realm:username , so a user who's username is javawizard and who's
+     * realm is opengroove.org would have the userid opengroove.org:javawizard .
+     * 
+     * @param realm
+     *            The name of the realm
+     * @param username
+     *            The username
+     * @return The corresponding userid
+     */
     public static String toUserid(String realm,
         String username)
     {
         return realm + ":" + username;
+    }
+    
+    /**
+     * Extracts the name of the user's realm from the userid.
+     * 
+     * @param userid
+     *            The userid
+     * @return The realm portion of the userid specified
+     */
+    public static String toRealm(String userid)
+    {
+        
+    }
+    
+    /**
+     * Extracts the user's username from the userid.
+     * 
+     * @param userid
+     *            The userid
+     * @return The username portion of the userid specified
+     */
+    public static String toUsername(String userid)
+    {
+        
     }
 }
---
+++ client/main/trunk/src/tests/Test024.java
@@ -0,0 +1,26 @@
+package tests;
+
+import javax.swing.JButton;
+
+/**
+ * Simple test class for printing out the class of a button's default border.
+ * 
+ * ...which turned out to be
+ * javax.swing.plaf.BorderUIResource$CompoundBorderUIResource
+ * 
+ * @author Alexander Boyd
+ * 
+ */
+public class Test024
+{
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        System.out.println(new JButton().getBorder()
+            .getClass().getName());
+    }
+    
+}
