68869dc62961624313be61fc4845919c3aa7532f
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-08-09 20:04:58 +0000

    Did a bit with Test027, and added Test028 which will be used to test out the breadcrumb
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@238 124d5c61-e844-0410-bd6f-bad94b02dd72

--- client/main/trunk/src/tests/Test027.java
+++ client/main/trunk/src/tests/Test027.java
@@ -1,13 +1,23 @@
 package tests;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Point;
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollBar;
 import javax.swing.JViewport;
+import javax.swing.border.LineBorder;
+
+import net.sf.opengroove.client.ui.ScrollButtons;
 
 public class Test027
 {
@@ -22,33 +32,76 @@ public class Test027
         JFrame frame = new JFrame();
         frame.setSize(300, 100);
         frame.setLocationRelativeTo(null);
-        JPanel panel = new JPanel();
+        final JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel,
             BoxLayout.X_AXIS));
-        for (int i = 0; i < 20; i++)
+        JPanel outer = new JPanel();
+        outer.setLayout(new BorderLayout());
+        for (int i = 0; i < 10; i++)
         {
-            panel
-                .add(new JButton("Test button " + (i + 1)));
+            JButton b = new JButton("Test button "
+                + (i + 1));
+            b
+                .setBorder(new LineBorder(Color.LIGHT_GRAY,
+                    1));
+            panel.add(b);
         }
-        JViewport viewport = new JViewport();
+        final JViewport viewport = new JViewport();
         viewport.setView(panel);
         viewport.setViewPosition(new Point(0, 0));
-        frame.getContentPane().add(viewport,
+        outer.add(viewport, BorderLayout.CENTER);
+        final ScrollButtons bar = new ScrollButtons(
+            ScrollButtons.Orientation.HORIZONTAL, 3);
+        bar.setMinimumSize(new Dimension(20, 10));
+        frame.getContentPane().add(outer,
             BorderLayout.NORTH);
+        outer.add(bar, BorderLayout.EAST);
+        viewport
+            .addComponentListener(new ComponentListener()
+            {
+                
+                @Override
+                public void componentHidden(ComponentEvent e)
+                {
+                    // TODO Auto-generated method stub
+                    
+                }
+                
+                @Override
+                public void componentMoved(ComponentEvent e)
+                {
+                    // TODO Auto-generated method stub
+                    
+                }
+                
+                @Override
+                public void componentResized(
+                    ComponentEvent e)
+                {
+                    bar.setMaximum(panel.getWidth()
+                        - viewport.getWidth());
+                    viewport.setViewPosition(new Point(bar
+                        .getValue(), 0));
+                }
+                
+                @Override
+                public void componentShown(ComponentEvent e)
+                {
+                    // TODO Auto-generated method stub
+                    
+                }
+            });
         frame.show();
-        while (true)
+        bar.addAdjustmentListener(new AdjustmentListener()
         {
-            Thread.sleep(20);
-            try
+            
+            @Override
+            public void adjustmentValueChanged(
+                AdjustmentEvent e)
             {
-                viewport.setViewPosition(new Point(viewport
-                    .getViewPosition().x + 1, 0));
+                viewport.setViewPosition(new Point(bar
+                    .getValue(), 0));
             }
-            catch (Exception exception)
-            {
-                exception.printStackTrace();
-            }
-        }
+        });
     }
-    
 }
---
+++ client/main/trunk/src/tests/Test028.java
@@ -0,0 +1,16 @@
+package tests;
+
+import javax.swing.JScrollBar;
+
+public class Test028
+{
+    
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+    }
+    
+}
