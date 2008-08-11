916902e74db50310123f7ec61db365dca3b15c36
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-08-11 03:25:07 +0000

    Added a class for testing out xml attributes with newlines
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@245 124d5c61-e844-0410-bd6f-bad94b02dd72

---
+++ client/main/trunk/src/tests/Test030.java
@@ -0,0 +1,15 @@
+package tests;
+
+public class Test030
+{
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        // This class is for testing out what happens when an XML attribute
+        // contains a newline. I'm assuming some sort of entity will be added.
+    }
+    
+}
