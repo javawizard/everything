1d74cff82487612c3634b3487eb9466b63e1f776
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-08-11 03:57:56 +0000

    Finished the test class with the results I expected: it inserts an entity (a numerical one, I think it was &#13; for a carriage return and &#11; for a newline) into the attribute value. I didn't test out attribute names but I don't think I'll need newlines in names.
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@246 124d5c61-e844-0410-bd6f-bad94b02dd72

--- client/main/trunk/src/tests/Test030.java
+++ client/main/trunk/src/tests/Test030.java
@@ -1,15 +1,40 @@
 package tests;
 
+import java.io.StringWriter;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Source;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
 public class Test030
 {
     
     /**
      * @param args
      */
-    public static void main(String[] args)
+    public static void main(String[] args) throws Throwable
     {
         // This class is for testing out what happens when an XML attribute
         // contains a newline. I'm assuming some sort of entity will be added.
+        Document doc = DocumentBuilderFactory.newInstance()
+            .newDocumentBuilder().newDocument();
+        Element newElement = doc
+            .createElement("testelement");
+        newElement
+            .setAttribute(
+                "testattribute",
+                "This is line 1.\r\nThis is line 2, started with rn and ending with n.\nThis is line 3.");
+        doc.appendChild(newElement);
+        Source source = new DOMSource(doc);
+        StringWriter writer = new StringWriter();
+        StreamResult result = new StreamResult(writer);
+        TransformerFactory.newInstance().newTransformer()
+            .transform(source, result);
+        System.out.println(writer.toString());
     }
-    
 }
