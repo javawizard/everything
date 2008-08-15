b667dc800b795e44e1571c1d4aec687539576b59
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-08-15 06:17:00 +0000

    Added getters and setters for PermissionModel
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@283 124d5c61-e844-0410-bd6f-bad94b02dd72

--- client/main/trunk/src/net/sf/opengroove/client/plugins/PermissionModel.java
+++ client/main/trunk/src/net/sf/opengroove/client/plugins/PermissionModel.java
@@ -5,4 +5,34 @@ public class PermissionModel
     private String name;
     private boolean required;
     private String description;
+    
+    public String getName()
+    {
+        return name;
+    }
+    
+    public boolean isRequired()
+    {
+        return required;
+    }
+    
+    public String getDescription()
+    {
+        return description;
+    }
+    
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    
+    public void setRequired(boolean required)
+    {
+        this.required = required;
+    }
+    
+    public void setDescription(String description)
+    {
+        this.description = description;
+    }
 }
