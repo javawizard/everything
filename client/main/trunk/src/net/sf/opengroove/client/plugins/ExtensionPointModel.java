147159e918193a696110a25f3eb208e6e719aaa6
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-08-15 06:35:37 +0000

    Added more getters/setters
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@285 124d5c61-e844-0410-bd6f-bad94b02dd72

--- client/main/trunk/src/net/sf/opengroove/client/plugins/ExtensionPointModel.java
+++ client/main/trunk/src/net/sf/opengroove/client/plugins/ExtensionPointModel.java
@@ -2,5 +2,39 @@ package net.sf.opengroove.client.plugins;
 
 public class ExtensionPointModel
 {
+    private String id;
+    private String extensionPointClass;
+    private String extensionInterface;
     
+    public String getId()
+    {
+        return id;
+    }
+    
+    public String getExtensionPointClass()
+    {
+        return extensionPointClass;
+    }
+    
+    public String getExtensionInterface()
+    {
+        return extensionInterface;
+    }
+    
+    public void setId(String id)
+    {
+        this.id = id;
+    }
+    
+    public void setExtensionPointClass(
+        String extensionPointClass)
+    {
+        this.extensionPointClass = extensionPointClass;
+    }
+    
+    public void setExtensionInterface(
+        String extensionInterface)
+    {
+        this.extensionInterface = extensionInterface;
+    }
 }
