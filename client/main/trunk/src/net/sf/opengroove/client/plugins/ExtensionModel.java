e1bd179ce6664c328ccccb0e727735e7e7e94905
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-08-15 06:18:33 +0000

    Added fields and getters/setters to ExtensionModel.java
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@284 124d5c61-e844-0410-bd6f-bad94b02dd72

--- client/main/trunk/src/net/sf/opengroove/client/plugins/ExtensionModel.java
+++ client/main/trunk/src/net/sf/opengroove/client/plugins/ExtensionModel.java
@@ -2,5 +2,40 @@ package net.sf.opengroove.client.plugins;
 
 public class ExtensionModel
 {
-    
+    private String id;
+    private String plugin;
+    private String point;
+    private String extensionClass;
+    public String getId()
+    {
+        return id;
+    }
+    public String getPlugin()
+    {
+        return plugin;
+    }
+    public String getPoint()
+    {
+        return point;
+    }
+    public String getExtensionClass()
+    {
+        return extensionClass;
+    }
+    public void setId(String id)
+    {
+        this.id = id;
+    }
+    public void setPlugin(String plugin)
+    {
+        this.plugin = plugin;
+    }
+    public void setPoint(String point)
+    {
+        this.point = point;
+    }
+    public void setExtensionClass(String extensionClass)
+    {
+        this.extensionClass = extensionClass;
+    }
 }
