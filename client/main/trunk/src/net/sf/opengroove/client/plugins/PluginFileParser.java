d8ed2049d8eefee892fba4b07f478e223d960e4f
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-08-15 03:05:32 +0000

    Added some fields to PluginModel, and added the new class PluginFileParser for parsing and generating plugin configuration files
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@280 124d5c61-e844-0410-bd6f-bad94b02dd72

---
+++ client/main/trunk/src/net/sf/opengroove/client/plugins/PluginFileParser.java
@@ -0,0 +1,13 @@
+package net.sf.opengroove.client.plugins;
+
+/**
+ * This class contains methods related to parsing and generating plugin
+ * configuration files.
+ * 
+ * @author Alexander Boyd
+ * 
+ */
+public class PluginFileParser
+{
+    
+}
---
+++ client/main/trunk/src/net/sf/opengroove/client/plugins/PluginModel.java
@@ -0,0 +1,8 @@
+package net.sf.opengroove.client.plugins;
+
+public class PluginModel
+{
+    private String name;
+    private String description;
+    private String license;
+}
