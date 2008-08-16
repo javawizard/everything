39f0b42f1e8a116bc5230347c464bc3ccc0d2257
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-08-16 19:59:19 +0000

    Added a speech example
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@318 124d5c61-e844-0410-bd6f-bad94b02dd72

---
+++ client/main/trunk/src/tests/Test032.java
@@ -0,0 +1,25 @@
+package tests;
+
+import com.sun.speech.freetts.Voice;
+import com.sun.speech.freetts.en.us.cmu_us_kal.KevinVoiceDirectory;
+
+public class Test032
+{
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args) throws Throwable
+    {
+        KevinVoiceDirectory dir = new KevinVoiceDirectory();
+        Voice voice = dir.getVoices()[1];
+        System.out.println("allocating");
+        voice.allocate();
+        System.out.println("allocated");
+        voice.setRate(130);
+        voice.setPitch(100);
+        voice.setPitchRange(25);
+        voice.speak("Hello. I am a talking machine. I can talk forever and ever without getting tired.");
+    }
+    
+}
