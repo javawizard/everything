27f565e17c2eb413ebd82d142b920d9df268a249
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-08-17 08:13:05 +0000

    Working on a UI test
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@333 124d5c61-e844-0410-bd6f-bad94b02dd72

---
+++ client/main/trunk/src/tests/Test034.java
@@ -0,0 +1,24 @@
+package tests;
+
+import javax.swing.JFrame;
+
+import com.jidesoft.swing.JideButton;
+
+import net.sf.opengroove.client.Statics;
+import net.sf.opengroove.client.ui.TestFrame;
+
+public class Test034
+{
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        Statics.run();
+        JFrame frame = new TestFrame();
+        JideButton button = new JideButton("Test button");
+        frame.getContentPane().add(button);
+        frame.show();
+    }
+}
