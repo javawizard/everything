c0d27010e245bc5344e4a857b9e19c767d9c9074
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-08-17 04:12:16 +0000

    More work on the test class (it's appearing to be successful so far)
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@325 124d5c61-e844-0410-bd6f-bad94b02dd72

--- client/main/trunk/src/tests/Test033.java
+++ client/main/trunk/src/tests/Test033.java
@@ -1,9 +1,21 @@
 package tests;
 
+import java.awt.Color;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowFocusListener;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 import javax.swing.JPopupMenu;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
 
 public class Test033
 {
@@ -19,17 +31,44 @@ public class Test033
     public static void main(String[] args)
     {
         // SwingPopupMenu menu = new SwingPopupMenu();
-        JFrame frame = new JFrame();
-        frame.getContentPane().add(
-            new JLabel("This is the test frame"));
+        final JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel,
+            BoxLayout.Y_AXIS));
+        panel.add(new JLabel("test label 1"));
+        panel.add(new JLabel("test label 2"));
+        panel.add(new JButton("test button 1"));
+        panel.add(new JButton("test button 2"));
+        panel.add(new JLabel("another label"));
+        panel.setBorder(new CompoundBorder(new LineBorder(
+            Color.GRAY, 1), new EmptyBorder(2, 2, 2, 2)));
+        frame.getContentPane().add(panel);
         frame.setUndecorated(true);
         frame.setAlwaysOnTop(true);
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.show();
+        frame.requestFocus();
+        frame
+            .addWindowFocusListener(new WindowFocusListener()
+            {
+                
+                @Override
+                public void windowGainedFocus(WindowEvent e)
+                {
+                    // TODO Auto-generated method stub
+                    
+                }
+                
+                @Override
+                public void windowLostFocus(WindowEvent e)
+                {
+                    frame.dispose();
+                    System.out.println("Frame disposed");
+                }
+            });
         // menu.add(item1);
         // menu.add(item2);
         // menu.show(null, -1,-1);
     }
-    
 }
