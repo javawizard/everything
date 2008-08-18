d45d0d41b2e6be38892369a702aad0d98f76aded
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-08-18 03:46:53 +0000

    Added a new, small, project, called FillEditor, for simple creation of background images. It will be used to define a bunch of the window backgrounds in OpenGroove.
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@337 124d5c61-e844-0410-bd6f-bad94b02dd72

---
+++ client/main/trunk/src/net/sf/opengroove/projects/filleditor/FillEditor.java
@@ -0,0 +1,15 @@
+package net.sf.opengroove.projects.filleditor;
+
+public class FillEditor
+{
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
