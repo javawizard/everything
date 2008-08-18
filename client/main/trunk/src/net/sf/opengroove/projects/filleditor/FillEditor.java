910aeb7d378cc4fa183866d61b37f29a7baa22d6
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-08-18 07:25:04 +0000

    Got the basic frame done, but about nothing else. About to try it for the first time.
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@356 124d5c61-e844-0410-bd6f-bad94b02dd72

--- client/main/trunk/src/net/sf/opengroove/projects/filleditor/FillEditor.java
+++ client/main/trunk/src/net/sf/opengroove/projects/filleditor/FillEditor.java
@@ -1,18 +1,42 @@
 package net.sf.opengroove.projects.filleditor;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
 
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
 
 public class FillEditor
 {
+    public static class PointSelection
+    {
+        /**
+         * If true, then this point is a parameter, and <code>region</code>
+         * and <code>parameter</code> apply. If not, this point is a region
+         * boundary point, and <code>region</code> and <code>index</code>
+         * apply.
+         */
+        public boolean isParameter;
+        public String parameter;
+        public Region region;
+        public int index;
+    }
+    
     private static JFrame frame;
     private static FillImage image;
-    private Point selectedPoint;
+    private static Point highlightedPoint;
+    private PointSelection selectedPoint;
     public static final int BOX_WIDTH = 8;
     public static final int BOX_HEIGHT = 8;
     public static final int HALF_BOX_WIDTH = BOX_WIDTH / 2;
@@ -21,31 +45,33 @@ public class FillEditor
         100, 100, 160);
     public static final Color BOX_INNER_2 = new Color(255,
         0, 0, 160);
-    private JComponent imageComponent = new JComponent()
+    private static JComponent imageComponent = new JComponent()
     {
         public void paintComponent(Graphics g)
         {
             Graphics2D g2 = (Graphics2D) g;
             if (image != null)
                 image.draw(g2, image.width, image.height);
-            if (selectedPoint != null)
+            if (highlightedPoint != null)
             {
                 // TODO: change to %2000 to enable pulsing
                 g2
                     .setColor((System.currentTimeMillis() % 1000) < 1000 ? BOX_INNER_1
                         : BOX_INNER_2);
-                g2.drawRect(selectedPoint.x
-                    - HALF_BOX_WIDTH, selectedPoint.y
+                g2.fillRect(highlightedPoint.x
+                    - HALF_BOX_WIDTH, highlightedPoint.y
                     - HALF_BOX_HEIGHT, BOX_WIDTH,
                     BOX_HEIGHT);
                 g2.setColor(new Color(0, 0, 0, 200));
-                g2.drawRect(selectedPoint.x
-                    - HALF_BOX_WIDTH, selectedPoint.y
+                g2.drawRect(highlightedPoint.x
+                    - HALF_BOX_WIDTH, highlightedPoint.y
                     - HALF_BOX_HEIGHT, BOX_WIDTH,
                     BOX_HEIGHT);
             }
         }
     };
+    
+    private static JPanel leftPanel = new JPanel();
     
     /**
      * @param args
@@ -53,16 +79,36 @@ public class FillEditor
     public static void main(String[] args)
     {
         frame = new JFrame("FillEditor - OpenGroove");
+        frame.getContentPane()
+            .setLayout(new BorderLayout());
+        frame.getContentPane().add(leftPanel,
+            BorderLayout.WEST);
+        leftPanel.setBorder(new CompoundBorder(
+            new EmptyBorder(2, 2, 2, 2),
+            new CompoundBorder(new LineBorder(Color.BLACK,
+                1), new EmptyBorder(2, 2, 2, 2))));
+        frame.getContentPane().add(
+            new JScrollPane(imageComponent),
+            BorderLayout.EAST);
+        frame.setSize(500, 400);
+        frame.setLocationRelativeTo(null);
+        frame.show();
     }
     
     /**
-     * Builds the editor onto the panel spec
+     * Builds the left column editor onto the panel specified.
      * 
      * @param panel
      */
     public static void buildEditor(JPanel panel)
     {
-        
+        panel.setLayout(new BoxLayout(panel,
+            BoxLayout.Y_AXIS));
+        JButton saveButton = new JButton("save");
+        panel.add(saveButton);
+        panel.add(new JLabel("Regions:"));
+        JButton addRegion = new JButton("Add");
+        panel.add(addRegion);
     }
     
 }
