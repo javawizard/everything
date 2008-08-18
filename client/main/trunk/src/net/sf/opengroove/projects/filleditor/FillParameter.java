3f2c353ef4c712b61ee30eef2d64cec639629766
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-08-18 05:05:51 +0000

    Still more work on FillEditor
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@342 124d5c61-e844-0410-bd6f-bad94b02dd72

--- client/main/trunk/src/net/sf/opengroove/projects/filleditor/FillParameter.java
+++ client/main/trunk/src/net/sf/opengroove/projects/filleditor/FillParameter.java
@@ -8,13 +8,36 @@ public class FillParameter implements Serializable
      * 
      */
     private static final long serialVersionUID = 2033861388607534050L;
+    
     public enum Type
     {
-        COLOR,POINT,FLOAT
+        COLOR, POINT, FLOAT
     }
-    private String name;
-    private Type type;
-    private int min;
-    private int max;
-    private Object value;
+    
+    /**
+     * The name of this parameter.
+     */
+    public String name;
+    /**
+     * The description of this parameter.
+     */
+    public String description;
+    /**
+     * The type of this parameter.
+     */
+    public Type type;
+    /**
+     * For types that have a defined range, this is the minimum value of the
+     * field.
+     */
+    public int min;
+    /**
+     * For types that have a defined range, this is the maximum value of the
+     * field.
+     */
+    public int max;
+    /**
+     * The actual value of the field.
+     */
+    public Object value;
 }
--- client/main/trunk/src/net/sf/opengroove/projects/filleditor/Region.java
+++ client/main/trunk/src/net/sf/opengroove/projects/filleditor/Region.java
@@ -9,6 +9,17 @@ public class Region implements java.io.Serializable
      * 
      */
     private static final long serialVersionUID = 3334537932557520969L;
-    private Point[] points;
-    private FillPlugin plugin;
+    /**
+     * The points in this region
+     */
+    public Point[] points;
+    /**
+     * The starting indexes of any sets of 3 points that should define a bezier
+     * curve. For example, if there are 6 points, and this array contains one
+     * element, namely the number 1, then the second, third, and fourth points
+     * will define a bezier curve, while the rest of the points will just define
+     * straight lines.
+     */
+    public int[] cubicBezier;
+    public FillPlugin plugin;
 }
