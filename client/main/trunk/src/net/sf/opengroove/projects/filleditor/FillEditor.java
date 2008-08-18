cb746c8423a8421404a3fc928f9c3402dbd8bfa7
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-08-18 07:15:34 +0000

    Still working on FillEditor
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@355 124d5c61-e844-0410-bd6f-bad94b02dd72

--- client/main/trunk/src/net/sf/opengroove/projects/filleditor/FillEditor.java
+++ client/main/trunk/src/net/sf/opengroove/projects/filleditor/FillEditor.java
@@ -1,5 +1,6 @@
 package net.sf.opengroove.projects.filleditor;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Point;
@@ -12,6 +13,14 @@ public class FillEditor
     private static JFrame frame;
     private static FillImage image;
     private Point selectedPoint;
+    public static final int BOX_WIDTH = 8;
+    public static final int BOX_HEIGHT = 8;
+    public static final int HALF_BOX_WIDTH = BOX_WIDTH / 2;
+    public static final int HALF_BOX_HEIGHT = BOX_HEIGHT / 2;
+    public static final Color BOX_INNER_1 = new Color(100,
+        100, 100, 160);
+    public static final Color BOX_INNER_2 = new Color(255,
+        0, 0, 160);
     private JComponent imageComponent = new JComponent()
     {
         public void paintComponent(Graphics g)
@@ -19,7 +28,22 @@ public class FillEditor
             Graphics2D g2 = (Graphics2D) g;
             if (image != null)
                 image.draw(g2, image.width, image.height);
-            g2.
+            if (selectedPoint != null)
+            {
+                // TODO: change to %2000 to enable pulsing
+                g2
+                    .setColor((System.currentTimeMillis() % 1000) < 1000 ? BOX_INNER_1
+                        : BOX_INNER_2);
+                g2.drawRect(selectedPoint.x
+                    - HALF_BOX_WIDTH, selectedPoint.y
+                    - HALF_BOX_HEIGHT, BOX_WIDTH,
+                    BOX_HEIGHT);
+                g2.setColor(new Color(0, 0, 0, 200));
+                g2.drawRect(selectedPoint.x
+                    - HALF_BOX_WIDTH, selectedPoint.y
+                    - HALF_BOX_HEIGHT, BOX_WIDTH,
+                    BOX_HEIGHT);
+            }
         }
     };
     
