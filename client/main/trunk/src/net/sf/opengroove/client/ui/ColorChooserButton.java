79c3362d6dc529312a699bb32050e8a951f5aaf6
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-08-18 08:53:28 +0000

    Finished the color chooser button. Time to write a test...
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@361 124d5c61-e844-0410-bd6f-bad94b02dd72

--- client/main/trunk/src/net/sf/opengroove/client/ui/ColorChooserButton.java
+++ client/main/trunk/src/net/sf/opengroove/client/ui/ColorChooserButton.java
@@ -1,22 +1,82 @@
 package net.sf.opengroove.client.ui;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.Icon;
 import javax.swing.JButton;
 import javax.swing.JColorChooser;
 import javax.swing.JPopupMenu;
 
 public class ColorChooserButton extends JButton
 {
-    private JColorChooser chooser;
+    private JColorChooser chooser = new JColorChooser();
+    
+    public JColorChooser getChooser()
+    {
+        return chooser;
+    }
+    
     private JPopupMenu menu;
     private JButton ok;
     
-    public ColorChooserButton()
+    public ColorChooserButton(Color color)
     {
-        chooser = new JColorChooser();
+        chooser.setColor(color);
+        setIcon(new Icon()
+        {
+            
+            @Override
+            public int getIconHeight()
+            {
+                return 12;
+            }
+            
+            @Override
+            public int getIconWidth()
+            {
+                return 16;
+            }
+            
+            @Override
+            public void paintIcon(Component c, Graphics g,
+                int x, int y)
+            {
+                g.setColor(chooser.getColor());
+                g.fillRect(x, y, getIconWidth(),
+                    getIconHeight());
+                g.setColor(Color.BLACK);
+                g.drawRect(x, y, getIconWidth(),
+                    getIconHeight());
+            }
+        });
         menu = new JPopupMenu();
         ok = new JButton("OK");
         menu.setLayout(new BorderLayout());
+        menu.add(ok, BorderLayout.SOUTH);
+        menu.add(chooser, BorderLayout.CENTER);
+        ok.addActionListener(new ActionListener()
+        {
+            
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                menu.hide();
+            }
+        });
+        addActionListener(new ActionListener()
+        {
+            
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                menu.show(ColorChooserButton.this, 0,
+                    ColorChooserButton.this.getHeight());
+            }
+        });
     }
 }
