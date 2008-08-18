7c48bce5d5776bf65e7effb9f370d8b94f98e734
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-08-18 08:55:29 +0000

    Wrote a test for the color chooser button
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@362 124d5c61-e844-0410-bd6f-bad94b02dd72

---
+++ client/main/trunk/src/tests/Test037.java
@@ -0,0 +1,21 @@
+package tests;
+
+import java.awt.Color;
+
+import net.sf.opengroove.client.ui.ColorChooserButton;
+import net.sf.opengroove.client.ui.TestFrame;
+
+public class Test037
+{
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        TestFrame frame = new TestFrame();
+        frame.getContentPane().add(
+            new ColorChooserButton(Color.RED));
+        frame.show();
+    }
+}
