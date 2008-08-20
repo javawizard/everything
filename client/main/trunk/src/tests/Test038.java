1df23c87d1e4beb124dbc250746c0e1ece95baec
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-08-20 20:22:59 +0000

    Created a test class for testing out wizards embedded within a JFrame
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@403 124d5c61-e844-0410-bd6f-bad94b02dd72

---
+++ client/main/trunk/src/tests/Test038.java
@@ -0,0 +1,15 @@
+package tests;
+
+public class Test038
+{
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
