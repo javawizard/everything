8902a4a77ce9b46a32118eb23b80c4cf2c7eed90
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-08-21 02:43:02 +0000

    Working on a new utility class, StandardWizardPage
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@416 124d5c61-e844-0410-bd6f-bad94b02dd72

---
+++ client/main/trunk/src/net/sf/opengroove/client/ui/StandardWizardPage.java
@@ -0,0 +1,14 @@
+package net.sf.opengroove.client.ui;
+
+import com.jidesoft.wizard.DefaultWizardPage;
+
+public class StandardWizardPage extends DefaultWizardPage
+{
+
+    public StandardWizardPage(String title)
+    {
+        super(title);
+        // TODO Auto-generated constructor stub
+    }
+    
+}
