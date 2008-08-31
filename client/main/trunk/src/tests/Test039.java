d0a187ef18c5c559f6deb9207463bda64eb01582
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-08-31 20:50:01 +0000

    Added a class for showing the value of a particular character
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@525 124d5c61-e844-0410-bd6f-bad94b02dd72

---
+++ client/main/trunk/src/tests/Test039.java
@@ -0,0 +1,16 @@
+package tests;
+
+import net.sf.opengroove.client.ui.TestFrame;
+
+public class Test039
+{
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        new TestFrame("⇨").show();
+    }
+    
+}
