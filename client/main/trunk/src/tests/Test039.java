ff9b22e50ad61ee63c761a426bd098b6a3ad66a7
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-08-31 21:02:03 +0000

    Improved the character test to show a list of all of the unicode arrow characters
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@526 124d5c61-e844-0410-bd6f-bad94b02dd72

--- client/main/trunk/src/tests/Test039.java
+++ client/main/trunk/src/tests/Test039.java
@@ -1,5 +1,17 @@
 package tests;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.MouseAdapter;
+
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
 import net.sf.opengroove.client.ui.TestFrame;
 
 public class Test039
@@ -10,7 +22,26 @@ public class Test039
      */
     public static void main(String[] args)
     {
-        new TestFrame("⇨").show();
+        TestFrame frame = new TestFrame();
+        frame.getContentPane()
+            .setLayout(new BorderLayout());
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(0, 10, 2, 2));
+        frame.getContentPane().add(new JScrollPane(panel));
+        frame.show();
+        for (int i = 0x2190; i < 0x2200; i++)
+        {
+            JLabel label = new JLabel(" " + (char) i + " ");
+            label.setFont(Font.decode("Dialog 18"));
+            label.setToolTipText("0x"
+                + Integer.toHexString(i));
+            panel.add(label);
+            panel.invalidate();
+            panel.validate();
+            panel.repaint();
+            frame.invalidate();
+            frame.validate();
+            frame.repaint();
+        }
     }
-    
 }
