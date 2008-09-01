47ca57902e51c194a39148f356233c1181af032f
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-09-01 16:09:48 +0000

    Working on the OgvFile class
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@548 124d5c61-e844-0410-bd6f-bad94b02dd72

--- client/main/trunk/src/net/sf/opengroove/client/com/OgvFile.java
+++ client/main/trunk/src/net/sf/opengroove/client/com/OgvFile.java
@@ -3,15 +3,34 @@ package net.sf.opengroove.client.com;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
 
 public class OgvFile
 {
-    public static Properties load(File file) throws FileNotFoundException, IOException
+    private Properties properties = new Properties();
+    
+    public OgvFile(File file) throws FileNotFoundException,
+        IOException
     {
-        Properties props = new Properties();
-        props.load(new FileInputStream(file));
-        return props;
+        properties.load(new FileInputStream(file));
+    }
+    
+    public void save(File file)
+        throws FileNotFoundException, IOException
+    {
+        properties.store(new FileOutputStream(file),
+            "OGVFile");
+    }
+    
+    public String getField(String name)
+    {
+        return properties.getProperty(name);
+    }
+    
+    public void setField(String name, String value)
+    {
+        properties.setProperty(name, value);
     }
 }
