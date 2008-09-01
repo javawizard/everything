ee93aadb3bc68489d7d1e9fd7d0d829683ee2703
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-09-01 00:07:04 +0000

    Added some more symbols
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@533 124d5c61-e844-0410-bd6f-bad94b02dd72

--- client/main/trunk/src/net/sf/opengroove/client/Symbol.java
+++ client/main/trunk/src/net/sf/opengroove/client/Symbol.java
@@ -1,13 +1,86 @@
 package net.sf.opengroove.client;
 
+/**
+ * A bunch of symbols that I've found that I think could be used in portions of
+ * OpenGroove. the toString() method on each symbol returns a string with the
+ * symbol itself, so you can just embed symbol enum constants directly within
+ * strings.
+ * 
+ * @author Alexander Boyd
+ * 
+ */
 public enum Symbol
 {
-    UP("↑"), LEFT("←"), RIGHT("→"), DOWN("↓");
+    /**
+     * The character &uarr;, an up-arrow.
+     */
+    UP("↑"),
+    /**
+     * The character &larr;, a left-arrow.
+     */
+    LEFT("←"),
+    /**
+     * The character &rarr;, a right-arrow.
+     */
+    RIGHT("→"),
+    /**
+     * The character &darr;, a down-arrow.
+     */
+    DOWN("↓"),
+    /**
+     * The character &#x2610;, a checkbox with no markings on it.
+     */
+    CHECKBOX(0x2610),
+    /**
+     * The character &#x2611;, a checkbox with a check mark on it.
+     */
+    CHECKBOX_CHECK(0x2611),
+    /**
+     * The character &#x2612;, a checkbox with an X on it.
+     */
+    CHECKBOX_X(0x2612),
+    /**
+     * The character &#x263a;, a face with a smile on it.
+     */
+    SMILE(0x263a),
+    /**
+     * The character &#x2654;, a white chess king.
+     */
+    CHESS_WHITE_KING(0x2654),
+    /**
+     * The character &#x2655;, a white chess queen.
+     */
+    CHESS_WHITE_QUEEN(0x2655),
+    /**
+     * The character &#x2656;, a white chess rook.
+     */
+    CHESS_WHITE_ROOK(0x2656),
+    /**
+     * The character &#x2657;, a white chess bishop.
+     */
+    CHESS_WHITE_BISHOP(0x2657),
+    /**
+     * The character &#x2658;, a white chess knight.
+     */
+    CHESS_WHITE_KNIGHT(0x2658),
+    /**
+     * The character &#x2659;, a white chess pawn.
+     */
+    CHESS_WHITE_PAWN(0x2659),
+    /**
+     * The character &#x265a;, a black chess king.
+     */
+    CHESS_BLACK_KING(0x265a);
     private String value;
     
     private Symbol(String value)
     {
         this.value = value;
+    }
+    
+    private Symbol(int value)
+    {
+        this.value = "" + (char) value;
     }
     
     public String toString()
