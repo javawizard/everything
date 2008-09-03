3df95cea9f2ef0a3c62d883eac04912276d3b4ff
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-09-03 03:19:40 +0000

    Finished up reference changing. Back to the new account wizard...
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@585 124d5c61-e844-0410-bd6f-bad94b02dd72

--- client/main/trunk/src/net/sf/opengroove/client/Storage.java
+++ client/main/trunk/src/net/sf/opengroove/client/Storage.java
@@ -19,7 +19,7 @@ import java.util.HashMap;
 import java.util.Hashtable;
 
 import net.sf.opengroove.client.workspace.WorkspaceWrapper;
-import net.sf.opengroove.security.Hash;
+import net.sf.opengroove.common.security.Hash;
 
 import base64.Base64Coder;
 
