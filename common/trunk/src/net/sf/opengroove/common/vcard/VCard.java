ea97a6383a97e4fc710637df95d7c4d0af18e44f
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-09-03 22:35:48 +0000

    Added a clas stub for what will be a class for parsing and generating vCard files
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@587 124d5c61-e844-0410-bd6f-bad94b02dd72

---
+++ common/trunk/src/net/sf/opengroove/common/vcard/VCard.java
@@ -0,0 +1,13 @@
+package net.sf.opengroove.common.vcard;
+
+/**
+ * A class that represents a vCard. It contains methods for parsing and
+ * outputting the vCard file format.
+ * 
+ * @author Alexander Boyd
+ * 
+ */
+public class VCard
+{
+    
+}
