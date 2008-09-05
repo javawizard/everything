644f26662b681bbfcf67e5e54916621ca4c00e5b
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-09-05 06:09:08 +0000

    Still working
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@594 124d5c61-e844-0410-bd6f-bad94b02dd72

--- client/main/trunk/src/net/sf/opengroove/client/ui/frames/UserNotificationFrame.java
+++ client/main/trunk/src/net/sf/opengroove/client/ui/frames/UserNotificationFrame.java
@@ -1,9 +1,11 @@
 package net.sf.opengroove.client.ui.frames;
+import javax.swing.BorderFactory;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 
@@ -29,6 +31,7 @@ public class UserNotificationFrame extends JFrame
     private JLabel jLabel3;
     private JLabel jLabel4;
     private JLabel jLabel5;
+    private JTextField subjectTextField;
     public UserNotificationFrame(long dateIssued,
         long dateExpires, Priority priority,
         String subject, String message)
@@ -54,27 +57,35 @@ public class UserNotificationFrame extends JFrame
                     getContentPane().add(jLabel2);
                     jLabel2.setText("Subject:");
                     jLabel2.setBounds(12, 68, 96, 20);
-                    jLabel2.setSize(100, 20);
+                    jLabel2.setSize(100, 26);
+                    jLabel2.setFont(new java.awt.Font("Dialog",1,16));
                 }
                 {
                     jLabel3 = new JLabel();
                     getContentPane().add(jLabel3);
                     jLabel3.setText("Issued on:");
-                    jLabel3.setBounds(12, 88, 100, 20);
+                    jLabel3.setBounds(12, 94, 100, 20);
                 }
                 {
                     jLabel4 = new JLabel();
                     getContentPane().add(jLabel4);
                     jLabel4.setText("Expires:");
-                    jLabel4.setBounds(12, 108, 100, 20);
+                    jLabel4.setBounds(12, 114, 100, 20);
                 }
                 {
                     jLabel5 = new JLabel();
                     getContentPane().add(jLabel5);
                     jLabel5.setText("Priority:");
-                    jLabel5.setBounds(12, 128, 44, 16);
+                    jLabel5.setBounds(12, 134, 100, 20);
                     jLabel5.setAutoscrolls(true);
-                    jLabel5.setSize(100, 20);
+                }
+                {
+                    subjectTextField = new JTextField();
+                    getContentPane().add(subjectTextField);
+                    subjectTextField.setBounds(112, 62, 220, 32);
+                    subjectTextField.setOpaque(false);
+                    subjectTextField.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
+                    subjectTextField.setEditable(false);
                 }
             }
             {
