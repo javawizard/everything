8366b3b9967f54372898830cdcc266818589e993
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-09-06 03:40:48 +0000

    Initial work on the ProxyStorage subsystem
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@614 124d5c61-e844-0410-bd6f-bad94b02dd72

---
+++ common/trunk/src/net/sf/opengroove/common/proxystorage/ProxyBean.java
@@ -0,0 +1,14 @@
+package net.sf.opengroove.common.proxystorage;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Interfaces that allow proxy storage should be annotated with this annotation.
+ * @author Alexander Boyd
+ *
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ProxyBean
+{
+}
