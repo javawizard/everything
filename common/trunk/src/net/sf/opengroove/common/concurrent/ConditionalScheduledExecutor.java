b09cdfbda574dd1f7ad677b590fbf3f7278fa0ae
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-09-10 19:38:25 +0000

    More work on ConditionalTimer
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@624 124d5c61-e844-0410-bd6f-bad94b02dd72

--- common/trunk/src/net/sf/opengroove/common/concurrent/ConditionalScheduledExecutor.java
+++
@@ -1,8 +0,0 @@
-package net.sf.opengroove.common.concurrent;
-
-import java.util.concurrent.ScheduledThreadPoolExecutor;
-
-public class ConditionalTimer extends Thread
-{
-    public abstract void execute();
-}
