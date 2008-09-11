d060a4824892d2938087f6d6df6393f7e51595c4
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-09-11 04:10:58 +0000

    More work on the notification api stuff
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@649 124d5c61-e844-0410-bd6f-bad94b02dd72

--- client/main/trunk/src/net/sf/opengroove/client/ui/transitions/NotificationFrameTransition.java
+++ client/main/trunk/src/net/sf/opengroove/client/ui/transitions/NotificationFrameTransition.java
@@ -1,5 +1,7 @@
 package net.sf.opengroove.client.ui.transitions;
 
+import javax.swing.JPanel;
+
 import net.sf.opengroove.client.notification.TaskbarNotificationFrame;
 
 /**
@@ -10,5 +12,41 @@ import net.sf.opengroove.client.notification.TaskbarNotificationFrame;
  */
 public interface NotificationFrameTransition
 {
+    /**
+     * Gets the number of steps that this notification requires.
+     * 
+     * @return
+     */
+    public int getStepCount();
     
+    /**
+     * Initializes this transition, and requests that the frame be initialized
+     * as well. This method should add the panel specified to the frame's
+     * content pane, or wrap it with it's own custom panel or container. The
+     * layout of the frame's content pane is whatever the default is for JFrame.
+     * At the time of this writing, the default is BorderLayout. The only
+     * requirement is that either the layout's preferred size always be equal
+     * (or greater) than the panel passed in, or that the {@link #apply(int)}
+     * method resize the frame to be the panel's preferred size at that time.
+     * 
+     * @param frame
+     *            The taskbar notification frame
+     * @param panel
+     *            The panel that contains the notification frame's contents
+     */
+    public void initialize(TaskbarNotificationFrame frame,
+        JPanel panel);
+    
+    /**
+     * Applies this notification to the frame specified in
+     * {@link #initialize(TaskbarNotificationFrame, JPanel)}, for the step
+     * specified. The step will range from 0 to getStepCount(), with 0 being
+     * completely hidden and getStepCount() being completely visible.<br/><br/>
+     * 
+     * This method will never be called before initialize().
+     * 
+     * @param step
+     *            The current step to apply
+     */
+    public void apply(int step);
 }
