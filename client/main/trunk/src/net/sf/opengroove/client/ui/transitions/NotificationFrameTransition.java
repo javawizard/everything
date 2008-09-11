06746c4bc24ab213c2f08ff93ab9d6966d1ae83a
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-09-11 03:48:22 +0000

    More transition stuff
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@647 124d5c61-e844-0410-bd6f-bad94b02dd72

---
+++ client/main/trunk/src/net/sf/opengroove/client/ui/transitions/NotificationFrameTransition.java
@@ -0,0 +1,14 @@
+package net.sf.opengroove.client.ui.transitions;
+
+import net.sf.opengroove.client.notification.TaskbarNotificationFrame;
+
+/**
+ * A transition for showing and hiding {@link TaskbarNotificationFrame}s.
+ * 
+ * @author Alexander Boyd
+ * 
+ */
+public interface NotificationFrameTransition
+{
+    
+}
