58df7aa5adad7ec750d8fb189fb0ecae1a526772
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-09-18 07:53:07 +0000

    Oh well, I still can't figure out the packetloss and corruption thing. I'll try tomorrow when I'm not so tired.
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@791 124d5c61-e844-0410-bd6f-bad94b02dd72

--- client/main/trunk/src/tests/Test043.java
+++ client/main/trunk/src/tests/Test043.java
@@ -16,32 +16,52 @@ public class Test043
      */
     public static void main(String[] args) throws Throwable
     {
-        for (int i = 0; i < 50000; i++)
+        BigInteger aesRandomNumber = new BigInteger(3060,
+            new SecureRandom());
+        byte[] securityKeyBytes = new byte[32];
+        System.arraycopy(aesRandomNumber.toByteArray(), 0,
+            securityKeyBytes, 0, 32);
+        final Aes256 key = new Aes256(securityKeyBytes);
+        final byte[] toEnc = ("Hello, world! How are you today? This "
+            + "is some very long text. Good-bye!")
+            .getBytes();
+        File testfile = new File("C:\\testfile.ogvts");
+        if (testfile.exists())
+            testfile.delete();
+        final FileOutputStream fos = new FileOutputStream(
+            testfile);
+        Thread[] threads = new Thread[10];
+        for (int i = 0; i < threads.length; i++)
         {
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            BigInteger aesRandomNumber = new BigInteger(
-                3060, new SecureRandom());
-            byte[] securityKeyBytes = new byte[32];
-            System.arraycopy(aesRandomNumber.toByteArray(),
-                0, securityKeyBytes, 0, 32);
-            final Aes256 securityKey = new Aes256(
-                securityKeyBytes);
-            BigInteger testRandomNumber = new BigInteger(
-                3060, new SecureRandom());
-            String testString = testRandomNumber
-                .toString(36);
-            testString = testString.substring(0,
-                (int) ((Math.random() * 200) + 3));
-            Crypto.enc(securityKey, testString.getBytes(),
-                baos);
-            ByteArrayInputStream is = new ByteArrayInputStream(
-                baos.toByteArray());
-            String dec = new String(Crypto.dec(securityKey,
-                is, 65535));
-            if (!dec.equals(testString))
-                throw new RuntimeException(
-                    "Invalid decryption, value is " + dec
-                        + " and enc value is " + testString);
+            threads[i] = new Thread()
+            {
+                public void run()
+                {
+                    for (int i = 0; i < 10000; i++)
+                    {
+                        try
+                        {
+                            Crypto.enc(key, toEnc, fos);
+                        }
+                        catch (IOException e)
+                        {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                }
+            };
         }
+        for (Thread thread : threads)
+        {
+            thread.start();
+        }
+        System.out
+            .println("waiting for writes to complete...");
+        for (Thread thread : threads)
+        {
+            thread.join();
+        }
+        System.out
+            .println("done, about to decrypt and validate...");
     }
 }
