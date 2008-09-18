fa8e1214a171731dedca810ab542e3d589189f51
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-09-18 02:50:00 +0000

    Nothing's turned up yet. I changed the program to run a bunch of times (curently 300) to see if that will help find the problem. If that doesn't work, I'll add some code to use a random length for the string, and probably random characters.
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@758 124d5c61-e844-0410-bd6f-bad94b02dd72

--- client/main/trunk/src/tests/Test043.java
+++ client/main/trunk/src/tests/Test043.java
@@ -16,19 +16,25 @@ public class Test043
      */
     public static void main(String[] args) throws Throwable
     {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        BigInteger aesRandomNumber = new BigInteger(3060,
-            new SecureRandom());
-        byte[] securityKeyBytes = new byte[32];
-        System.arraycopy(aesRandomNumber.toByteArray(), 0,
-            securityKeyBytes, 0, 32);
-        final Aes256 securityKey = new Aes256(
-            securityKeyBytes);
-        Crypto.enc(securityKey, "Hello world!".getBytes(),
-            baos);
-        ByteArrayInputStream is = new ByteArrayInputStream(
-            baos.toByteArray());
-        System.out.println(new String(Crypto.dec(
-            securityKey, is, 65535)));
+        for (int i = 0; i < 300; i++)
+        {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            BigInteger aesRandomNumber = new BigInteger(
+                3060, new SecureRandom());
+            byte[] securityKeyBytes = new byte[32];
+            System.arraycopy(aesRandomNumber.toByteArray(),
+                0, securityKeyBytes, 0, 32);
+            final Aes256 securityKey = new Aes256(
+                securityKeyBytes);
+            Crypto.enc(securityKey, "Hello world!"
+                .getBytes(), baos);
+            ByteArrayInputStream is = new ByteArrayInputStream(
+                baos.toByteArray());
+            String dec = new String(Crypto.dec(securityKey,
+                is, 65535));
+            if (!dec.equals("Hello world!"))
+                throw new RuntimeException(
+                    "Invalid decryption, value is " + dec);
+        }
     }
 }
