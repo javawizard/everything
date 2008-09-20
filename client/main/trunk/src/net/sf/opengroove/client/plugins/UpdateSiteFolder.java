0dd7167817b135a2f9ec3e7edb64b562be2dd759
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-09-20 04:22:20 +0000

    More work on UpdateSiteFolder, and removed UpdatePath as it is no longer needed with the way I'm doing UpdateSiteFolder
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@806 124d5c61-e844-0410-bd6f-bad94b02dd72

--- client/main/trunk/src/net/sf/opengroove/client/plugins/UpdateSiteFolder.java
+++ client/main/trunk/src/net/sf/opengroove/client/plugins/UpdateSiteFolder.java
@@ -1,5 +1,11 @@
 package net.sf.opengroove.client.plugins;
 
+import java.net.URL;
+
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
+
 /**
  * A class that connects to an update site. It is passed the url of an update
  * site, and can be queried for the update site's root folder, and the folders
@@ -22,6 +28,27 @@ public class UpdateSiteFolder
      * UPDATE: This stuff will actually use ReferableNode, which handles file
      * referencing, so we don't need to worry about URL stuff anymore.
      */
-    
+
     private ReferableNode node;
+    
+    public UpdateSiteFolder(URL url)
+    {
+        try
+        {
+            Element element = new SAXBuilder().build(url)
+                .getRootElement();
+            this.node = new ReferableNode(element);
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException(
+                "Exception occured while creating folder for site "
+                    + url, e);
+        }
+    }
+    
+    private UpdateSiteFolder(ReferableNode node)
+    {
+        
+    }
 }
