e8136b71862b67e6a8220fedafcfdfbb02eb12d6
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-09-20 23:06:27 +0000

    I'm finally learning how to create icons!
    Anyway, I created two new icons, add and delete. On the next commit I'll promote them out of the inprogress folder, or at least the .png versions.
    
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@815 124d5c61-e844-0410-bd6f-bad94b02dd72

--- client/main/trunk/src/tests/Test044.java
+++ client/main/trunk/src/tests/Test044.java
@@ -2,6 +2,7 @@ package tests;
 
 import java.awt.Insets;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.UIManager;
 
@@ -17,8 +18,8 @@ public class Test044
      */
     public static void main(String[] args) throws Throwable
     {
-        UIManager.setLookAndFeel(UIManager
-            .getSystemLookAndFeelClassName());
+//        UIManager.setLookAndFeel(UIManager
+//            .getSystemLookAndFeelClassName());
         for (Icons icon : Icons.values())
         {
             icon.setImage(OpenGroove.scaleImage(OpenGroove
@@ -26,12 +27,15 @@ public class Test044
                 .getSize(), icon.getSize()));
         }
         TestFrame frame = new TestFrame();
-        JButton button = new JButton("Test button");
+        JButton button = new JButton(new ImageIcon(
+            OpenGroove.Icons.GENERIC_ADD_16.getImage()));
         button.setFocusable(false);
         button.setMargin(new Insets(0, 0, 0, 0));
         frame.add(button);
-        JButton button2 = new JButton("Test button");
+        JButton button2 = new JButton(new ImageIcon(
+            OpenGroove.Icons.GENERIC_CANCEL_16.getImage()));
         button2.setFocusable(false);
+        button2.setMargin(new Insets(0, 0, 0, 0));
         frame.add(button2);
         frame.show();
     }
