5032aa2110e84a930b07c177621b02c99efc2353
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-09-23 08:33:51 +0000

    Last commit of the night
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@831 124d5c61-e844-0410-bd6f-bad94b02dd72

--- client/main/trunk/src/net/sf/opengroove/client/dynamics/EngineWriter.java
+++ client/main/trunk/src/net/sf/opengroove/client/dynamics/EngineWriter.java
@@ -2,5 +2,24 @@ package net.sf.opengroove.client.dynamics;
 
 public interface EngineWriter
 {
+    /**
+     * Indicates to the engine writer that the accessor is done writing to it.
+     * Any methods that write to it should throw an IllegalStateException after
+     * this is called. In addition, the date that this is called should be
+     * marked as the date for the resulting delta that can be taken from this
+     * writer.
+     */
+    public void finish();
     
+    /**
+     * Returns this writer's delta. This throws an IllegalStateException if
+     * finish() has not been called yet. If it has, then this method returns the
+     * delta...
+     * 
+     * TODO: should this method exist? Or should calling finish() flush the
+     * commands over to the context directly? 
+     * 
+     * @return
+     */
+    public Delta getDelta();
 }
