5ee0924eab829048019b9e91e5b1e1f2dd4a63ab
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-09-23 05:18:03 +0000

    Added a ton more classes related to dynamics
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@828 124d5c61-e844-0410-bd6f-bad94b02dd72

--- client/main/trunk/src/net/sf/opengroove/client/dynamics/EngineProvider.java
+++ client/main/trunk/src/net/sf/opengroove/client/dynamics/EngineProvider.java
@@ -13,7 +13,17 @@ package net.sf.opengroove.client.dynamics;
  */
 public interface EngineProvider
 {
-    public EngineReader createReader();
+    /**
+     * Creates a new EngineReader. If <code>live</code> is true, then the
+     * engine reader should keep it's contents up-to-date with the contents of
+     * the engine itself, but the reader might block on a particular operation
+     * if the engine is in the process of applying a delta. If it is false, then
+     * the engine reader obtained provides a snapshot of the data at the time
+     * this method was called.
+     * 
+     * @return
+     */
+    public EngineReader createReader(boolean live);
+    
     public EngineWriter createWriter();
-    public 
 }
