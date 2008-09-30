65ebeb5247cf323529c2807fb413d9914e525139
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-09-30 05:32:08 +0000

    Working on a persistence test
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@866 124d5c61-e844-0410-bd6f-bad94b02dd72

---
+++ client/main/trunk/src/tests/Test046.java
@@ -0,0 +1,39 @@
+package tests;
+
+import java.util.HashMap;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.hibernate.Hibernate;
+import org.hibernate.ejb.HibernatePersistence;
+
+/**
+ * A class for testing out simple persistant stuff.
+ * 
+ * @author Alexander Boyd
+ * 
+ */
+public class Test046
+{
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        HashMap map = new HashMap();
+        map.put("hibernate.dialect",
+            "org.hibernate.dialect.H2Dialect");
+        map.put("hibernate.hbm2ddl.auto", "update");
+        map.put("hibernate.connection.driver_class",
+            "org.h2.Driver");
+        map.put("hibernate.connection.username", "sa");
+        map.put("hibernate.connection.password", "");
+        map.put("hibernate.connection.url",
+            "jdbc:h2:sandbox/test046/db");
+        EntityManagerFactory factory = Persistence
+            .createEntityManagerFactory("testunit", map);
+    }
+    
+}
