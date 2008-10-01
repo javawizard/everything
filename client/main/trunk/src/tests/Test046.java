097beac74152b80a2f8525311c0eefefd92cbf01
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-10-01 00:57:26 +0000

    I am THIS CLOSE to going berserk and chucking my computer out the window...
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@871 124d5c61-e844-0410-bd6f-bad94b02dd72

--- client/main/trunk/src/tests/Test046.java
+++ client/main/trunk/src/tests/Test046.java
@@ -1,6 +1,9 @@
 package tests;
 
+import java.io.File;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Properties;
@@ -43,108 +46,122 @@ public class Test046
         map.put("hibernate.connection.url",
             "jdbc:h2:sandbox/test046/db");
         EntityManagerFactory factory = new HibernatePersistence()
-            .createContainerEntityManagerFactory(new PersistenceUnitInfo(){
-
-                @Override
-                public void addTransformer(
-                    ClassTransformer transformer)
+            .createContainerEntityManagerFactory(
+                new PersistenceUnitInfo()
                 {
-                    // TODO Auto-generated method stub
                     
-                }
-
-                @Override
-                public boolean excludeUnlistedClasses()
-                {
-                    // TODO Auto-generated method stub
-                    return false;
-                }
-
-                @Override
-                public ClassLoader getClassLoader()
-                {
-                    // TODO Auto-generated method stub
-                    return null;
-                }
-
-                @Override
-                public List<URL> getJarFileUrls()
-                {
-                    // TODO Auto-generated method stub
-                    return null;
-                }
-
-                @Override
-                public DataSource getJtaDataSource()
-                {
-                    // TODO Auto-generated method stub
-                    return null;
-                }
-
-                @Override
-                public List<String> getManagedClassNames()
-                {
-                    // TODO Auto-generated method stub
-                    return null;
-                }
-
-                @Override
-                public List<String> getMappingFileNames()
-                {
-                    // TODO Auto-generated method stub
-                    return null;
-                }
-
-                @Override
-                public ClassLoader getNewTempClassLoader()
-                {
-                    // TODO Auto-generated method stub
-                    return null;
-                }
-
-                @Override
-                public DataSource getNonJtaDataSource()
-                {
-                    // TODO Auto-generated method stub
-                    return null;
-                }
-
-                @Override
-                public String getPersistenceProviderClassName()
-                {
-                    // TODO Auto-generated method stub
-                    return null;
-                }
-
-                @Override
-                public String getPersistenceUnitName()
-                {
-                    // TODO Auto-generated method stub
-                    return null;
-                }
-
-                @Override
-                public URL getPersistenceUnitRootUrl()
-                {
-                    // TODO Auto-generated method stub
-                    return null;
-                }
-
-                @Override
-                public Properties getProperties()
-                {
-                    // TODO Auto-generated method stub
-                    return null;
-                }
-
-                @Override
-                public PersistenceUnitTransactionType getTransactionType()
-                {
-                    // TODO Auto-generated method stub
-                    return null;
-                }}, map);
-        EntityManager manager = factory 
+                    @Override
+                    public void addTransformer(
+                        ClassTransformer transformer)
+                    {
+                        // TODO Auto-generated method stub
+                        
+                    }
+                    
+                    @Override
+                    public boolean excludeUnlistedClasses()
+                    {
+                        // TODO Auto-generated method stub
+                        return false;
+                    }
+                    
+                    @Override
+                    public ClassLoader getClassLoader()
+                    {
+                        // TODO Auto-generated method stub
+                        return super.getClass()
+                            .getClassLoader();
+                    }
+                    
+                    @Override
+                    public List<URL> getJarFileUrls()
+                    {
+                        return new ArrayList<URL>();
+                    }
+                    
+                    @Override
+                    public DataSource getJtaDataSource()
+                    {
+                        // TODO Auto-generated method stub
+                        return null;
+                    }
+                    
+                    @Override
+                    public List<String> getManagedClassNames()
+                    {
+                        // TODO Auto-generated method stub
+                        return null;
+                    }
+                    
+                    @Override
+                    public List<String> getMappingFileNames()
+                    {
+                        // TODO Auto-generated method stub
+                        return null;
+                    }
+                    
+                    @Override
+                    public ClassLoader getNewTempClassLoader()
+                    {
+                        // TODO Auto-generated method stub
+                        return null;
+                    }
+                    
+                    @Override
+                    public DataSource getNonJtaDataSource()
+                    {
+                        // TODO Auto-generated method stub
+                        return null;
+                    }
+                    
+                    @Override
+                    public String getPersistenceProviderClassName()
+                    {
+                        // TODO Auto-generated method stub
+                        return null;
+                    }
+                    
+                    @Override
+                    public String getPersistenceUnitName()
+                    {
+                        // TODO Auto-generated method stub
+                        return "default";
+                    }
+                    
+                    @Override
+                    public URL getPersistenceUnitRootUrl()
+                    {
+                        try
+                        {
+                            return new File("classes")
+                                .toURI().toURL();
+                        }
+                        catch (MalformedURLException e)
+                        {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+                        return null;
+                    }
+                    
+                    @Override
+                    public Properties getProperties()
+                    {
+                        // TODO Auto-generated method stub
+                        return new Properties();
+                    }
+                    
+                    @Override
+                    public PersistenceUnitTransactionType getTransactionType()
+                    {
+                        // TODO Auto-generated method stub
+                        return null;
+                    }
+                }, map);
+        System.out.println("creating entitymanager");
+        EntityManager manager = factory
             .createEntityManager();
+        System.out.println("created.");
         TestMessage message = new TestMessage();
         message.setName("testname");
         message.setMessage("This is a test message.");
