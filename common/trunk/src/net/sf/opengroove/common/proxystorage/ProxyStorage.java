80b692891998690fbca9c2a8f876042b77b9d156
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-10-01 03:55:23 +0000

    I decided that JPA is just too heavyweight for what OpenGroove needs. I'm going with the ProxyStorage system for now.
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@873 124d5c61-e844-0410-bd6f-bad94b02dd72

--- common/trunk/src/net/sf/opengroove/common/proxystorage/ProxyStorage.java
+++ common/trunk/src/net/sf/opengroove/common/proxystorage/ProxyStorage.java
@@ -1,6 +1,56 @@
 package net.sf.opengroove.common.proxystorage;
 
-public class ProxyStorage
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * The ProxyStorage class is a class used for storing simple java beans to disk,
+ * in such a way that updating one instance of a particular persistant bean
+ * causes all other instances within the JVM to immediately reflect the new
+ * state. ProxyStorage is intended to be a replacement for Java Persistence API,
+ * when JPA is just too heavyweight.
+ * 
+ * Each ProxyStorage instance has a root object. The root object is the single
+ * object to be persisted. It should have various fields that other objects can
+ * be placed on to make them persistant.
+ * 
+ * When an object is created, it is entered into the proxy storage system with
+ * no parent. It can then be assigned to fields of other persistent objects as
+ * necessary. Objects that are in the database but which do not have the storage
+ * root as an ancestor are removed upon calling the purge method of
+ * ProxyStorage. The purge method generally should only be called right after
+ * the proxy storage is created but before it goes into use, as it will remove
+ * any objects that are not currently in the tree of objects, which could
+ * include a newly-created object that hasn't been assigned to the tree yet.
+ * 
+ * @author Alexander Boyd
+ * 
+ * @param <E>
+ *            The class of the root of the storage.
+ */
+public class ProxyStorage<E>
 {
+    private Connection connection;
     
+    private Class<E> root;
+    
+    public ProxyStorage(Class<E> root, File location)
+        throws SQLException
+    {
+        try
+        {
+            Class.forName("org.h2.Driver");
+        }
+        catch (ClassNotFoundException e)
+        {
+            throw new IllegalStateException(
+                "The H2Database jar is not on your classpath. H2 is "
+                    + "used as the backend for ProxyStorage, "
+                    + "and so is required.", e);
+        }
+        connection = DriverManager.getConnection("jdbc:h2:"
+            + location.getPath());
+    }
 }
