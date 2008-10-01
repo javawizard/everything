f7c1da00761b541744047d054190489fd0047630
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-10-01 00:50:19 +0000

    I am going to be driven slowly insane while trying to figure out how to get JPA to run without a persistence.xml file...
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@870 124d5c61-e844-0410-bd6f-bad94b02dd72

--- client/main/trunk/src/tests/Test046.java
+++ client/main/trunk/src/tests/Test046.java
@@ -1,12 +1,22 @@
 package tests;
 
+import java.net.URL;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Properties;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.spi.ClassTransformer;
+import javax.persistence.spi.PersistenceUnitInfo;
+import javax.persistence.spi.PersistenceUnitTransactionType;
+import javax.sql.DataSource;
 
 import org.hibernate.Hibernate;
 import org.hibernate.ejb.HibernatePersistence;
+
+import tests.test046.TestMessage;
 
 /**
  * A class for testing out simple persistant stuff.
@@ -33,7 +43,112 @@ public class Test046
         map.put("hibernate.connection.url",
             "jdbc:h2:sandbox/test046/db");
         EntityManagerFactory factory = new HibernatePersistence()
-            .createEntityManagerFactory("testunit", map);
+            .createContainerEntityManagerFactory(new PersistenceUnitInfo(){
+
+                @Override
+                public void addTransformer(
+                    ClassTransformer transformer)
+                {
+                    // TODO Auto-generated method stub
+                    
+                }
+
+                @Override
+                public boolean excludeUnlistedClasses()
+                {
+                    // TODO Auto-generated method stub
+                    return false;
+                }
+
+                @Override
+                public ClassLoader getClassLoader()
+                {
+                    // TODO Auto-generated method stub
+                    return null;
+                }
+
+                @Override
+                public List<URL> getJarFileUrls()
+                {
+                    // TODO Auto-generated method stub
+                    return null;
+                }
+
+                @Override
+                public DataSource getJtaDataSource()
+                {
+                    // TODO Auto-generated method stub
+                    return null;
+                }
+
+                @Override
+                public List<String> getManagedClassNames()
+                {
+                    // TODO Auto-generated method stub
+                    return null;
+                }
+
+                @Override
+                public List<String> getMappingFileNames()
+                {
+                    // TODO Auto-generated method stub
+                    return null;
+                }
+
+                @Override
+                public ClassLoader getNewTempClassLoader()
+                {
+                    // TODO Auto-generated method stub
+                    return null;
+                }
+
+                @Override
+                public DataSource getNonJtaDataSource()
+                {
+                    // TODO Auto-generated method stub
+                    return null;
+                }
+
+                @Override
+                public String getPersistenceProviderClassName()
+                {
+                    // TODO Auto-generated method stub
+                    return null;
+                }
+
+                @Override
+                public String getPersistenceUnitName()
+                {
+                    // TODO Auto-generated method stub
+                    return null;
+                }
+
+                @Override
+                public URL getPersistenceUnitRootUrl()
+                {
+                    // TODO Auto-generated method stub
+                    return null;
+                }
+
+                @Override
+                public Properties getProperties()
+                {
+                    // TODO Auto-generated method stub
+                    return null;
+                }
+
+                @Override
+                public PersistenceUnitTransactionType getTransactionType()
+                {
+                    // TODO Auto-generated method stub
+                    return null;
+                }}, map);
+        EntityManager manager = factory 
+            .createEntityManager();
+        TestMessage message = new TestMessage();
+        message.setName("testname");
+        message.setMessage("This is a test message.");
+        manager.persist(message);
     }
     
 }
--- client/main/trunk/src/tests/test046/TestMessage.java
+++ client/main/trunk/src/tests/test046/TestMessage.java
@@ -1,26 +1,36 @@
 package tests.test046;
 
+import javax.persistence.Basic;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 @Entity
 public class TestMessage
 {
     @Id
     private String name;
+    @Basic(fetch = FetchType.LAZY)
     private String message;
+    
     public String getName()
     {
         return name;
     }
+    
     public String getMessage()
     {
         return message;
     }
+    
     public void setName(String name)
     {
         this.name = name;
     }
+    
     public void setMessage(String message)
     {
         this.message = message;
