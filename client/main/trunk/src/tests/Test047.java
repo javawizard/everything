2574d7edb9ae25963d6067f5c90f8e27aa65ea49
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-10-02 21:19:49 +0000

    Added a test to print out info about a class
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@891 124d5c61-e844-0410-bd6f-bad94b02dd72

---
+++ client/main/trunk/src/tests/Test047.java
@@ -0,0 +1,20 @@
+package tests;
+
+import net.sf.opengroove.client.OpenGroove;
+
+public class Test047
+{
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("name:             "
+            + OpenGroove.class.getName());
+        System.out.println("canonical name:   "
+            + OpenGroove.class.getCanonicalName());
+        System.out.println("simple name:      "
+            + OpenGroove.class.getSimpleName());
+    }
+}
