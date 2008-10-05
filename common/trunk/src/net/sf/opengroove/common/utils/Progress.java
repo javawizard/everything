62c89b2893c94edca82744a0f2ed09cc38eb0b71
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-10-05 05:23:10 +0000

    Added some actual content to the Progress class
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@924 124d5c61-e844-0410-bd6f-bad94b02dd72

--- common/trunk/src/net/sf/opengroove/common/utils/Progress.java
+++ common/trunk/src/net/sf/opengroove/common/utils/Progress.java
@@ -1,9 +1,28 @@
 package net.sf.opengroove.common.utils;
 
 import net.sf.opengroove.client.com.ListenerManager;
+import net.sf.opengroove.client.com.Notifier;
 
 public class Progress
 {
     private double value = 0;
     private ListenerManager<ProgressListener> listeners = new ListenerManager<ProgressListener>();
+    
+    public void set(double value)
+    {
+        if (value < 0)
+            value = 0;
+        if (value > 1)
+            value = 1;
+        this.value = value;
+        listeners.notify(new Notifier<ProgressListener>()
+        {
+            
+            @Override
+            public void notify(ProgressListener listener)
+            {
+                listener.progressUpdated(Progress.this);
+            }
+        });
+    }
 }
