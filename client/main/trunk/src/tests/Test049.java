90e97173a9267a68b73f4b753d58425eeebf0bc8
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-10-06 03:39:01 +0000

    Added vacuuming test
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@940 124d5c61-e844-0410-bd6f-bad94b02dd72

---
+++ client/main/trunk/src/tests/Test049.java
@@ -0,0 +1,24 @@
+package tests;
+
+import java.io.File;
+
+import net.sf.opengroove.common.proxystorage.ProxyStorage;
+import tests.test046.TestMessageList;
+
+public class Test049
+{
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args) throws Throwable
+    {
+        /*
+         * Test for vacuuming the database after Test046 is run
+         */
+        ProxyStorage<TestMessageList> storage = new ProxyStorage<TestMessageList>(
+            TestMessageList.class, new File(
+                "sandbox/test046/db"));
+    }
+    
+}
