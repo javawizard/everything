74acfad670c7078bd0d4cd93dcceac2a4c6421ad
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-10-09 04:46:59 +0000

    Added the delimited() methods that have been floating around to StringUtils
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@991 124d5c61-e844-0410-bd6f-bad94b02dd72

--- common/trunk/src/net/sf/opengroove/common/utils/StringUtils.java
+++ common/trunk/src/net/sf/opengroove/common/utils/StringUtils.java
@@ -1,23 +1,47 @@
 package net.sf.opengroove.common.utils;
 
+import net.sf.opengroove.common.proxystorage.ProxyStorage.ToString;
+
 public class StringUtils
 {
-    public static boolean isMemberOf(String string, String[] strings)
+    public static boolean isMemberOf(String string,
+        String[] strings)
     {
-        for(String test : strings)
+        for (String test : strings)
         {
-            if(test.equals(string))
+            if (test.equals(string))
                 return true;
         }
         return false;
     }
-    public static boolean isMemberOfIgnoreCase(String string, String[] strings)
+    
+    public static boolean isMemberOfIgnoreCase(
+        String string, String[] strings)
     {
-        for(String test : strings)
+        for (String test : strings)
         {
-            if(test.equalsIgnoreCase(string))
+            if (test.equalsIgnoreCase(string))
                 return true;
         }
         return false;
     }
+    
+    public static <T> String delimited(T[] items,
+        ToString<T> generator, String delimiter)
+    {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < items.length; i++)
+        {
+            if (i != 0)
+                sb.append(delimiter);
+            sb.append(generator.toString(items[i]));
+        }
+        return sb.toString();
+    }
+    
+    public interface ToString<S>
+    {
+        public String toString(S object);
+    }
+    
 }
