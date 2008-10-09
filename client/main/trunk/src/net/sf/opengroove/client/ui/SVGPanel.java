c5b6834f4571b760c98fa997bc0785ef70a2c34a
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-10-09 04:49:38 +0000

    Despite the obvious reduction in graphics quality when switching from SCALE_AREA_AVERAGING to VALUE_INTERPOLATION_BICUBIC, area averaging is proving to be just too slow for what I need to do. Changed SVGPanel to use bicubic interpolation instead. Hopefully I'll be able to figure out how to make JSVGCanvas do the scaling for me, which will remove this entire problem while greatly improving rendering quality.
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@993 124d5c61-e844-0410-bd6f-bad94b02dd72

--- client/main/trunk/src/net/sf/opengroove/client/ui/SVGPanel.java
+++ client/main/trunk/src/net/sf/opengroove/client/ui/SVGPanel.java
@@ -5,6 +5,7 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.RenderingHints;
 import java.awt.geom.Dimension2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -135,12 +136,17 @@ public class SVGPanel extends JPanel
                 .getWidth(), (int) svgDocumentSize
                 .getHeight()));
         Graphics2D ig = image.createGraphics();
+        ig.setRenderingHint(
+            RenderingHints.KEY_INTERPOLATION,
+            RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        ig.setRenderingHint(
+            RenderingHints.KEY_ANTIALIASING,
+            RenderingHints.VALUE_ANTIALIAS_ON);
         ig.setColor(new Color(0, 0, 0, 0));
         ig.fillRect(0, 0, image.getWidth(), image
             .getHeight());
         canvas.paint(ig);
-        g.drawImage(image.getScaledInstance(getWidth(),
-            getHeight(), Image.SCALE_AREA_AVERAGING), 0, 0,
-            getWidth(), getHeight(), null);
+        g.drawImage(image, 0, 0, getWidth(), getHeight(),
+            null);
     }
 }
