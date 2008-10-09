836848f8140b6ac77007d23e11991cb8aec81a79
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-10-09 18:40:24 +0000

    Well, it kind of worked. The menus were laid out in a completely whack layout, but they activated when clicked, which leads me to believe that the problem with the layout is my lack of experience with TableLayout.
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@1003 124d5c61-e844-0410-bd6f-bad94b02dd72

--- client/main/trunk/src/tests/Test050.java
+++ client/main/trunk/src/tests/Test050.java
@@ -1,6 +1,12 @@
 package tests;
 
+import java.awt.BorderLayout;
+
+import info.clearthought.layout.TableLayout;
+
+import javax.swing.JLabel;
 import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
 import net.sf.opengroove.client.IMenu;
@@ -21,9 +27,19 @@ public class Test050
         JMenu menu2 = new IMenu("Testmenu2",
             new JMenuItem[] { new JMenuItem("Item 1"),
                 new JMenuItem("Item 2") });
-        menu2.set
-        frame.getContentPane().add(menu);
-        frame.getContentPane().add(menu2);
+        JMenuBar bar = new JMenuBar();
+        bar.setLayout(new TableLayout(new double[] {
+            TableLayout.PREFERRED, TableLayout.PREFERRED,
+            TableLayout.FILL }, new double[] {
+            TableLayout.PREFERRED, TableLayout.FILL }));
+        JLabel lowerLabel = new JLabel("Alex Boyd");
+        bar.add(menu, "0, 0");
+        bar.add(menu2, "0, 1");
+        bar.add(new JLabel(""), "0, 2, c, c");
+        bar.add(lowerLabel, "1, 0, 1, 2, c, c");
+        frame.getContentPane()
+            .setLayout(new BorderLayout());
+        frame.getContentPane().add(bar);
         frame.show();
     }
     
