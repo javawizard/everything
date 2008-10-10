a0778c6198454e3c0e8a9ee7b9fd42cb6f8615c8
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-10-10 19:57:53 +0000

    Added 2 utility classes that I'm now thinking I'm going to remove
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@1018 124d5c61-e844-0410-bd6f-bad94b02dd72

---
+++ client/main/trunk/src/net/sf/opengroove/client/ui/BackgroundSVGPanel.java
@@ -0,0 +1,12 @@
+package net.sf.opengroove.client.ui;
+
+import javax.swing.JComponent;
+/**
+ * A class that draws an SVG image in the background, like SVGPanel, 
+ * @author Alexander Boyd
+ *
+ */
+public class BackgroundSVGPanel extends JComponent
+{
+    
+}
