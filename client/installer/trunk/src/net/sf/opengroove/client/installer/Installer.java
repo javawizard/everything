fff5750f9a7002be5ce7902b56577121c6f8fe75
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-10-11 19:20:36 +0000

    Got some initial info for the installer up
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@1032 124d5c61-e844-0410-bd6f-bad94b02dd72

--- client/installer/trunk/src/net/sf/opengroove/client/installer/Installer.java
+++ client/installer/trunk/src/net/sf/opengroove/client/installer/Installer.java
@@ -4,13 +4,16 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.io.File;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextArea;
 import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 
 public class Installer
@@ -20,7 +23,7 @@ public class Installer
     /**
      * @param args
      */
-    public static void main(String[] args)
+    public static void main(String[] args)throws Throwable
     {
         JFrame frame = new JFrame();
         frame.setSize(500, 550);
@@ -37,10 +40,29 @@ public class Installer
             .setLayout(new BorderLayout());
         JPanel top = new JPanel();
         top.setLayout(new BoxLayout(top, BoxLayout.Y_AXIS));
+        top.setBorder(new EmptyBorder(10, 10, 10, 10));
         frame.getContentPane().add(top, BorderLayout.NORTH);
-        top.add(Box.createVerticalStrut(15));
+        top.add(Box.createVerticalStrut(5));
         top.add(mainLabel);
         top.add(Box.createVerticalStrut(15));
+        JTextArea installDescription = new JTextArea();
+        installDescription.setEditable(false);
+        installDescription.setOpaque(false);
+        installDescription.setLineWrap(true);
+        installDescription.setWrapStyleWord(true);
+        installDescription
+            .append("Welcome to the OpenGroove Installer. This program will "
+                + "install OpenGroove on your computer. If you're running "
+                + "Windows Vista, you'll need to install the program somewhere "
+                + "that you have permission to modify, such as in your home "
+                + "folder (for example, "
+                + new File(System.getProperty("user.home"), "opengroove")
+                    .getCanonicalPath()
+                + "), not your "
+                + "Program Files folder. Choose where you want to install " +
+                		"OpenGroove, ");
+        installDescription.setAlignmentX(0);
+        top.add(installDescription);
         frame.show();
     }
 }
