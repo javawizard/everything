75d5ecf76b98a90aa9e9bc47b154e9da67381e98
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-10-11 06:22:49 +0000

    Added the main class
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@1029 124d5c61-e844-0410-bd6f-bad94b02dd72

---
+++ client/installer/trunk/src/net/sf/opengroove/client/installer/Installer.java
@@ -0,0 +1,15 @@
+package net.sf.opengroove.client.installer;
+
+public class Installer
+{
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
