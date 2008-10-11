2653cac1a774a382c9c7a8d5fb7b97ba7ba220cf
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-10-11 06:37:19 +0000

    Beginning to get fed up with BoxLayout...
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@1030 124d5c61-e844-0410-bd6f-bad94b02dd72

--- client/installer/trunk/src/net/sf/opengroove/client/installer/Installer.java
+++ client/installer/trunk/src/net/sf/opengroove/client/installer/Installer.java
@@ -1,15 +1,45 @@
 package net.sf.opengroove.client.installer;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.LineBorder;
+
 public class Installer
 {
+    private static final String DOWNLOAD_URL = "http://sysup.ogis.opengroove.org";
     
     /**
      * @param args
      */
     public static void main(String[] args)
     {
-        // TODO Auto-generated method stub
-        
+        JFrame frame = new JFrame();
+        frame.setSize(500, 450);
+        frame.setLocationRelativeTo(null);
+        JLabel mainLabel = new JLabel(
+            "Welcome to the OpenGroove Installer");
+        mainLabel.setFont(Font.decode(null).deriveFont(22f)
+            .deriveFont(Font.BOLD));
+        mainLabel
+            .setHorizontalAlignment(SwingConstants.CENTER);
+        frame.getContentPane()
+            .setLayout(new BorderLayout());
+        JPanel top = new JPanel();
+        top.setLayout(new BoxLayout(top, BoxLayout.Y_AXIS));
+        JPanel topWrapper = new JPanel();
+        topWrapper.setLayout()
+        frame.getContentPane().add(topWrapper, BorderLayout.NORTH);
+        top.add(Box.createVerticalStrut(15));
+        top.add(mainLabel);
+        top.add(Box.createVerticalStrut(15));
+        frame.show();
     }
-    
 }
