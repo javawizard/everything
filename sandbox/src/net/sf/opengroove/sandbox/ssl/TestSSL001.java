e24cf43048a3fd2a4da826cbcbd12b22b8a4e374
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-10-16 05:04:49 +0000

    Working on a class for testing out SSL sockets
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@1069 124d5c61-e844-0410-bd6f-bad94b02dd72

---
+++ sandbox/src/net/sf/opengroove/sandbox/ssl/TestSSL001.java
@@ -0,0 +1,15 @@
+package net.sf.opengroove.sandbox.ssl;
+
+public class TestSSL001
+{
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
