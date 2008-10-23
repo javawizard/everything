ea38d8b6db2b7aa8f72ee55637be51026a82ff8e
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-10-23 18:05:18 +0000

    Working on changing HelpButton
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@1070 124d5c61-e844-0410-bd6f-bad94b02dd72

--- client/main/trunk/src/net/sf/opengroove/client/help/HelpButton.java
+++ client/main/trunk/src/net/sf/opengroove/client/help/HelpButton.java
@@ -6,32 +6,48 @@ import java.awt.event.ActionListener;
 
 import net.sf.opengroove.client.OpenGroove;
 
+import com.jidesoft.swing.JideButton;
 import com.l2fprod.common.swing.JLinkButton;
 
-public class HelpButton extends JLinkButton
+/**
+ * A button that shows up as a link and, when clicked, opens a help viewer to
+ * the specified help topic.<br/><br/>
+ * 
+ * A note to plugin implementors: If you want to get acess to the help viewer
+ * that shows the global help (a help viewer is needed when creating this
+ * button), then call
+ * {@link net.sf.opengroove.client.OpenGroove#getGlobalHelpViewer()}. If you
+ * want access to the help viewer that views help for a particular user, call
+ * {@link net.sf.opengroove.client.OpenGroove#getHelpViewer(String)}, passing
+ * in the userid of the user in question, which will usually be provided for you
+ * by the extension point that you're extending.
+ * 
+ * @author Alexander Boyd
+ * 
+ */
+public class HelpButton extends JideButton
 {
-	/**
-	 * creates a help button that, when clicked, shows the help referenced by
-	 * the help path specified.
-	 * 
-	 * @param name
-	 *            the label, or text, that shows up on this button
-	 * @param path
-	 *            the path to the help. this is passed to
-	 *            OpenGroove.showHelpTopic()
-	 */
-	public HelpButton(String name, final String path)
-	{
-		super("<html><u>" + name + "</u>");
-		setBorder(null);
-		setForeground(Color.BLUE);
-		addActionListener(new ActionListener()
-		{
-
-			public void actionPerformed(ActionEvent e)
-			{
-				OpenGroove.showHelpTopic(path);
-			}
-		});
-	}
+    /**
+     * creates a help button that, when clicked, shows the help referenced by
+     * the help path specified.
+     * 
+     * @param name
+     *            the label, or text, that shows up on this button
+     * @param path
+     *            the path to the help. this is passed to
+     *            OpenGroove.showHelpTopic()
+     */
+    public HelpButton(final HelpViewer viewer, String name,
+        final String path)
+    {
+        super();
+        addActionListener(new ActionListener()
+        {
+            
+            public void actionPerformed(ActionEvent e)
+            {
+                OpenGroove.showHelpTopic(path);
+            }
+        });
+    }
 }
