7b408f9315634bfad26c42e7eac06ae55d215faa
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-10-24 23:16:12 +0000

    Added BouncyCastle to the libraries
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@1076 124d5c61-e844-0410-bd6f-bad94b02dd72

---
+++ sandbox/src/net/sf/opengroove/sandbox/ssl/TestSSL002.java
@@ -0,0 +1,21 @@
+package net.sf.opengroove.sandbox.ssl;
+
+/**
+ * Generates a self-signed certificate, and places it in a keystore located at
+ * test.jks
+ * 
+ * @author Alexander Boyd
+ * 
+ */
+public class TestSSL002
+{
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        
+    }
+    
+}
