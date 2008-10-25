d596db481a7eb77aa2962da07b4eabb712efaf83
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-10-25 05:36:40 +0000

    Finished up Test004. Now we'll create Test005, a class that will connect to the server socket hosted by Test004 and validate it's certificate.
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@1085 124d5c61-e844-0410-bd6f-bad94b02dd72

--- sandbox/src/net/sf/opengroove/sandbox/ssl/TestSSL004.java
+++ sandbox/src/net/sf/opengroove/sandbox/ssl/TestSSL004.java
@@ -1,13 +1,19 @@
 package net.sf.opengroove.sandbox.ssl;
 
 import java.io.FileInputStream;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.security.KeyStore;
 import java.security.Principal;
 import java.security.PrivateKey;
+import java.security.SecureRandom;
 import java.security.cert.Certificate;
 import java.security.cert.X509Certificate;
 
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509KeyManager;
 
 /**
@@ -80,6 +86,15 @@ public class TestSSL004
                 return null;
             }
         };
+        SSLContext context = SSLContext.getInstance("TLS");
+        context.init(new KeyManager[] { manager },
+            new TrustManager[] {}, new SecureRandom());
+        SSLServerSocketFactory factory = context
+            .getServerSocketFactory();
+        ServerSocket ss = factory.createServerSocket(24680);
+        System.out.println("about to accept");
+        Socket s = ss.accept();
+        System.out.println("accepted");
     }
     
 }
