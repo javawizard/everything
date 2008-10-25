bcd743efec2232e72d5065604b6855128f87090c
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-10-25 05:30:34 +0000

    Working on a class for serving SSLServerSockets now
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@1083 124d5c61-e844-0410-bd6f-bad94b02dd72

--- sandbox/src/net/sf/opengroove/sandbox/ssl/TestSSL004.java
+++ sandbox/src/net/sf/opengroove/sandbox/ssl/TestSSL004.java
@@ -1,5 +1,19 @@
 package net.sf.opengroove.sandbox.ssl;
 
+import java.net.Socket;
+import java.security.Principal;
+import java.security.PrivateKey;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.X509KeyManager;
+
+/**
+ * A class for creating a server socket that serves the certificate chain stored
+ * in example.jks
+ * 
+ * @author Alexander Boyd
+ * 
+ */
 public class TestSSL004
 {
     
@@ -8,8 +22,53 @@ public class TestSSL004
      */
     public static void main(String[] args)
     {
-        // TODO Auto-generated method stub
-        
+        X509KeyManager manager = new X509KeyManager()
+        {
+            
+            @Override
+            public String chooseClientAlias(
+                String[] keyType, Principal[] issuers,
+                Socket socket)
+            {
+                return "key";
+            }
+            
+            @Override
+            public String chooseServerAlias(String keyType,
+                Principal[] issuers, Socket socket)
+            {
+                return "key";
+            }
+            
+            @Override
+            public X509Certificate[] getCertificateChain(
+                String alias)
+            {
+                // TODO Auto-generated method stub
+                return null;
+            }
+            
+            @Override
+            public String[] getClientAliases(
+                String keyType, Principal[] issuers)
+            {
+                return null;
+            }
+            
+            @Override
+            public PrivateKey getPrivateKey(String alias)
+            {
+                // TODO Auto-generated method stub
+                return null;
+            }
+            
+            @Override
+            public String[] getServerAliases(
+                String keyType, Principal[] issuers)
+            {
+                return null;
+            }
+        };
     }
     
 }
