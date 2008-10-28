79c17feaab3e401d5e9c3cc0005b2aaa2cac8cd2
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-10-28 03:28:58 +0000

    Working on some more test classes, this time to test out PKCS#10 CSRs and signing them. If all works out, I'll use the PKCS#10 format for generating CSRs from within an OpenGroove Realm Server, and the JKS format for returning a new certificate.
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@1094 124d5c61-e844-0410-bd6f-bad94b02dd72

---
+++ sandbox/src/net/sf/opengroove/sandbox/ssl/TestSSL006.java
@@ -0,0 +1,15 @@
+package net.sf.opengroove.sandbox.ssl;
+
+public class TestSSL006
+{
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
