a33bd3e4a416ad7dd9245fe65ed3037734e74ee2
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-11-02 08:20:20 +0000

    Actually starting to work on GWT now
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@1118 124d5c61-e844-0410-bd6f-bad94b02dd72

---
+++ server2/realmserver/gwt-src/net/sf/opengroove/realmserver/gwt/core/AdminInterface.java
@@ -0,0 +1,51 @@
+package net.sf.opengroove.realmserver.gwt.core;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.ClickListener;
+import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.PasswordTextBox;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.Widget;
+
+public class AdminInterface implements EntryPoint
+{
+    
+    @Override
+    public void onModuleLoad()
+    {
+        RootPanel root = RootPanel.get();
+        HTMLPanel loginPanel = new HTMLPanel(
+            "<table border='0' cellpadding='2'>"
+                + "<tr><td colspan='2'>Please enter your username and "
+                + "password to log in."
+                + "</td></tr>"
+                + "<tr><td><b>Username: </b></td><td>"
+                + "<div id='usernameField'/></td></tr>"
+                + "<tr><td><b>Password: </b></td><td>"
+                + "<div id='passwordField'/></td></tr>"
+                + "<tr><td>&nbsp;</td><td>"
+                + "<div id='loginButton'/></td></tr></table>");
+        final TextBox usernameField = new TextBox();
+        usernameField.setVisibleLength(20);
+        loginPanel.add(usernameField, "usernameField");
+        PasswordTextBox passwordField = new PasswordTextBox();
+        passwordField.setVisibleLength(20);
+        loginPanel.add(passwordField, "passwordField");
+        Button loginButton = new Button("Log in");
+        loginButton.addClickListener(new ClickListener()
+        {
+            
+            @Override
+            public void onClick(Widget sender)
+            {
+                Window.setTitle("Logging in as "
+                    + usernameField.getText());
+            }
+        });
+        loginPanel.add(loginButton, "loginButton");
+    }
+    
+}
