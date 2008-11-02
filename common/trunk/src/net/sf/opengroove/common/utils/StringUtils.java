5fd316f1eae18d31d80c575ed7e28673fc0b6114
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-11-02 00:46:50 +0000

    Added methods for reading and writing files as strings
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@1110 124d5c61-e844-0410-bd6f-bad94b02dd72

--- common/trunk/src/net/sf/opengroove/common/utils/StringUtils.java
+++ common/trunk/src/net/sf/opengroove/common/utils/StringUtils.java
@@ -1,5 +1,14 @@
 package net.sf.opengroove.common.utils;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import net.sf.opengroove.common.proxystorage.ProxyStorage.ToString;
 
 public class StringUtils
@@ -42,6 +51,86 @@ public class StringUtils
     public interface ToString<S>
     {
         public String toString(S object);
+    }
+    
+    /**
+     * reads the file specified in to a string. the file must not be larger than
+     * 5 MB.
+     * 
+     * @param file.
+     * @return
+     */
+    public static String readFile(File file)
+    {
+        try
+        {
+            if (file.length() > (5 * 1000 * 1000))
+                throw new RuntimeException(
+                    "the file is "
+                        + file.length()
+                        + " bytes. that is too large. it can't be larger than 5000000 bytes.");
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            FileInputStream fis = new FileInputStream(file);
+            copy(fis, baos);
+            fis.close();
+            baos.flush();
+            baos.close();
+            return new String(baos.toByteArray(), "UTF-8");
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    /**
+     * Writes the string specified to the file specified.
+     * 
+     * @param string
+     *            A string to write
+     * @param file
+     *            The file to write <code>string</code> to
+     */
+    public static void writeFile(String string, File file)
+    {
+        try
+        {
+            ByteArrayInputStream bais = new ByteArrayInputStream(
+                string.getBytes("UTF-8"));
+            FileOutputStream fos = new FileOutputStream(
+                file);
+            copy(bais, fos);
+            bais.close();
+            fos.flush();
+            fos.close();
+        }
+        catch (Exception e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    /**
+     * Copies the contents of one stream to another. Bytes from the source
+     * stream are read until it is empty, and written to the destination stream.
+     * Neither the source nor the destination streams are flushed or closed.
+     * 
+     * @param in
+     *            The source stream
+     * @param out
+     *            The destination stream
+     * @throws IOException
+     *             if an I/O error occurs
+     */
+    public static void copy(InputStream in, OutputStream out)
+        throws IOException
+    {
+        byte[] buffer = new byte[8192];
+        int amount;
+        while ((amount = in.read(buffer)) != -1)
+        {
+            out.write(buffer, 0, amount);
+        }
     }
     
 }
