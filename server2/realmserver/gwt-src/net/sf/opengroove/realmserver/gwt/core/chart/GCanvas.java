052932d36de6ff2f4a86ccb9858265a10d36d3f6
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-11-14 02:55:48 +0000

    Hmm, I might be able to use this class as an absolute canvas...
    Actually, in the future, I should probably use some sort of SVG canvas...
    
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@1177 124d5c61-e844-0410-bd6f-bad94b02dd72

--- server2/realmserver/gwt-src/net/sf/opengroove/realmserver/gwt/core/chart/GCanvas.java
+++ server2/realmserver/gwt-src/net/sf/opengroove/realmserver/gwt/core/chart/GCanvas.java
@@ -2,60 +2,25 @@ package net.sf.opengroove.realmserver.gwt.core.chart;
 
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 
-public class GCanvas extends Widget
+/**
+ * A canvas for drawing on.
+ * 
+ * @author Alexander Boyd
+ * 
+ */
+public class GCanvas extends Composite
 {
-    private String idPrefix;
-    private Element tableElement;
     
     public GCanvas(int width, int height,
         String defaultColor)
     {
-        idPrefix = DOM.createUniqueId();
-        tableElement = DOM.createTable();
-        tableElement.setId(DOM.createUniqueId());
-        tableElement.setAttribute("border", "0");
-        tableElement.setAttribute("cellspacing", "0");
-        tableElement.setAttribute("cellpadding", "0");
-        setElement(tableElement);
-        for (int r = 0; r < height; r++)
-        {
-            Element rowElement = DOM.createTR();
-            rowElement.setId(DOM.createUniqueId());
-            rowElement.setAttribute("style", "height: 1px");
-            tableElement.appendChild(rowElement);
-            for (int c = 0; c < width; c++)
-            {
-                Element colElement = DOM.createTD();
-                colElement.setId(getId(c, r));
-                colElement.setAttribute("style",
-                    "width: 1px; height: 1px");
-                colElement.setAttribute("bgcolor",
-                    defaultColor);
-                rowElement.appendChild(colElement);
-            }
-        }
-    }
-    
-    private String getId(int x, int y)
-    {
-        return idPrefix + "-" + x + "-" + y;
     }
     
     public void fillRect(String color, int x, int y,
         int width, int height)
     {
-        for (int cx = x; cx < x + width; cx++)
-        {
-            for (int cy = y; cy < y + height; cy++)
-            {
-                Element cElement = DOM
-                    .getElementById(getId(cx, cy));
-                if (cElement == null)
-                    continue;
-                cElement.setAttribute("bgcolor", color);
-            }
-        }
     }
 }
