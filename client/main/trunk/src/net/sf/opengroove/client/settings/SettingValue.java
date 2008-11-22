0b3a0166f3f7db0b6cbf5eaed5a4922b284cc1f7
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-11-22 23:38:31 +0000

    Added content to SettingValue (a concrete class this time, instead of an interface)
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@1236 124d5c61-e844-0410-bd6f-bad94b02dd72

--- client/main/trunk/src/net/sf/opengroove/client/settings/SettingValue.java
+++ client/main/trunk/src/net/sf/opengroove/client/settings/SettingValue.java
@@ -2,5 +2,58 @@ package net.sf.opengroove.client.settings;
 
 public class SettingValue
 {
-    
+    private SettingSpec spec;
+    private String stringValue;
+    private int intValue;
+    private long longValue;
+    private double doubleValue;
+    private boolean booleanValue;
+    public SettingSpec getSpec()
+    {
+        return spec;
+    }
+    public String getStringValue()
+    {
+        return stringValue;
+    }
+    public int getIntValue()
+    {
+        return intValue;
+    }
+    public long getLongValue()
+    {
+        return longValue;
+    }
+    public double getDoubleValue()
+    {
+        return doubleValue;
+    }
+    public boolean isBooleanValue()
+    {
+        return booleanValue;
+    }
+    public void setSpec(SettingSpec spec)
+    {
+        this.spec = spec;
+    }
+    public void setStringValue(String stringValue)
+    {
+        this.stringValue = stringValue;
+    }
+    public void setIntValue(int intValue)
+    {
+        this.intValue = intValue;
+    }
+    public void setLongValue(long longValue)
+    {
+        this.longValue = longValue;
+    }
+    public void setDoubleValue(double doubleValue)
+    {
+        this.doubleValue = doubleValue;
+    }
+    public void setBooleanValue(boolean booleanValue)
+    {
+        this.booleanValue = booleanValue;
+    }
 }
