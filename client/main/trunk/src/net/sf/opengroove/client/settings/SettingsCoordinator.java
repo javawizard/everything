3569232e6ec1d9f94aa0b2eb25b7cab6d3b23eb0
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-11-22 04:50:15 +0000

    More work on settings stuff
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@1228 124d5c61-e844-0410-bd6f-bad94b02dd72

---
+++ client/main/trunk/src/net/sf/opengroove/client/settings/SettingsCoordinator.java
@@ -0,0 +1,18 @@
+package net.sf.opengroove.client.settings;
+
+import net.sf.opengroove.common.proxystorage.Constructor;
+import net.sf.opengroove.common.proxystorage.ListType;
+import net.sf.opengroove.common.proxystorage.Property;
+import net.sf.opengroove.common.proxystorage.ProxyBean;
+import net.sf.opengroove.common.proxystorage.StoredList;
+
+@ProxyBean
+public interface SettingsCoordinator
+{
+    @Property
+    @ListType(SettingValue.class)
+    public StoredList<SettingValue> getSettings();
+    @Constructor
+    public SettingValue createSettingValue();
+    
+}
---
+++ client/main/trunk/src/net/sf/opengroove/client/settings/SettingsManager.java
@@ -0,0 +1,15 @@
+package net.sf.opengroove.client.settings;
+
+/**
+ * A class that manages settings for a particular user. It is supplied upon
+ * creation with a SettingsPersistence object, a proxy storage bean which is
+ * typically stored as part of a LocalUser or as part of the general local
+ * storage.
+ * 
+ * @author Alexander Boyd
+ * 
+ */
+public class SettingsManager
+{
+    
+}
