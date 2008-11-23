f24071df59d184711651c3e9c9a23a3d854134f0
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-11-23 00:30:44 +0000

    Added some default setting types
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@1240 124d5c61-e844-0410-bd6f-bad94b02dd72

---
+++ client/main/trunk/src/net/sf/opengroove/client/settings/types/CheckboxParameters.java
@@ -0,0 +1,19 @@
+package net.sf.opengroove.client.settings.types;
+
+import net.sf.opengroove.client.settings.SettingParameters;
+
+public class CheckboxParameters implements
+    SettingParameters
+{
+    private boolean isInverted;
+    
+    public boolean isInverted()
+    {
+        return isInverted;
+    }
+    
+    public void setInverted(boolean isInverted)
+    {
+        this.isInverted = isInverted;
+    }
+}
--- client/main/trunk/src/net/sf/opengroove/client/settings/types/TextType.java
+++ client/main/trunk/src/net/sf/opengroove/client/settings/types/TextType.java
@@ -1,53 +1,65 @@
 package net.sf.opengroove.client.settings.types;
 
 import javax.swing.JComponent;
+import javax.swing.JTextField;
 
 import net.sf.opengroove.client.settings.SettingParameters;
 import net.sf.opengroove.client.settings.SettingType;
 import net.sf.opengroove.client.settings.SettingValue;
 
+/**
+ * A SettingType that allows the user to enter arbitrary text into a text box.
+ * It's value type is {@link String}. It does not require any parameters.
+ * 
+ * @author Alexander Boyd
+ * 
+ */
 public class TextType implements SettingType
 {
     
     public JComponent createComponent(
         SettingParameters parameters)
     {
-        // TODO Auto-generated method stub
-        return null;
+        return new JTextField();
     }
     
     public JComponent createReadOnlyComponent(
         JComponent component, SettingParameters parameters)
     {
-        // TODO Auto-generated method stub
-        return null;
+        JTextField field = new JTextField();
+        field.setEditable(false);
+        field.setText(((JTextField) component).getText());
+        return field;
     }
     
     public Object getValue(SettingValue value,
         SettingParameters parameters)
     {
-        // TODO Auto-generated method stub
-        return null;
+        if (value.getStringValue() == null)
+            return "";
+        return value.getStringValue();
     }
     
     public void loadValue(JComponent component,
         SettingValue value, SettingParameters parameters)
     {
-        // TODO Auto-generated method stub
-        
+        ((JTextField) component).setText(value
+            .getStringValue());
     }
     
     public void storeValue(JComponent component,
         SettingValue value, SettingParameters parameters)
     {
-        // TODO Auto-generated method stub
-        
+        value.setStringValue(((JTextField) component)
+            .getText());
     }
     
     public String validate(JComponent component,
         SettingParameters parameters)
     {
-        // TODO Auto-generated method stub
+        /*
+         * We don't need to perform any validation here
+         */
         return null;
     }
     
