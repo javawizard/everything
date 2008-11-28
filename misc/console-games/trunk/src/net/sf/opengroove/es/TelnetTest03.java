965b65c7225066d0d4042a3e4837d6bb720bf902
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-11-28 22:28:56 +0000

    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@1293 124d5c61-e844-0410-bd6f-bad94b02dd72

--- misc/console-games/trunk/src/net/sf/opengroove/es/TelnetTest03.java
+++ misc/console-games/trunk/src/net/sf/opengroove/es/TelnetTest03.java
@@ -1,5 +1,6 @@
 package net.sf.opengroove.es;
 
+import java.io.IOException;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -8,11 +9,15 @@ import net.sf.opengroove.es.utils.InteractiveConsole;
 
 public class TelnetTest03
 {
+    private static String spaceString = "";
     
     /**
      * @param args
+     * @throws IOException
+     * @throws InterruptedException
      */
     public static void main(String[] args)
+        throws IOException, InterruptedException
     {
         ServerSocket ss = new ServerSocket(36574);
         while (true)
@@ -23,12 +28,23 @@ public class TelnetTest03
             InteractiveConsole console = new InteractiveConsole(
                 out);
             console.setup();
+            console.setColor(console.GREEN);
+            for (int c = 0; c < 60; c++)
+            {
+                spaceString += " ";
+            }
+            for (int r = 0; r < 25; r++)
+            {
+                console.setPos(r, 1);
+                console.write(spaceString);
+            }
+            console.write("Hello.");
             Thread.sleep(2000);
             socket.close();
             ss.close();
             System.exit(0);
         }
-
+        
     }
     
 }
