a522421aeab03e2cc6c6f62dd041334d870344f1
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-11-29 11:08:54 +0000

    More changes to the message composition frame.
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@1303 124d5c61-e844-0410-bd6f-bad94b02dd72

--- client/main/trunk/src/net/sf/opengroove/client/ui/frames/ComposeMessageFrame.java
+++ client/main/trunk/src/net/sf/opengroove/client/ui/frames/ComposeMessageFrame.java
@@ -1,10 +1,13 @@
 package net.sf.opengroove.client.ui.frames;
 
+import com.jidesoft.swing.JideButton;
+
 import info.clearthought.layout.TableLayout;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import javax.swing.JScrollPane;
 import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
@@ -30,6 +33,18 @@ public class ComposeMessageFrame extends javax.swing.JFrame
     private JLabel jLabel2;
     private JLabel jLabel3;
     private JPanel messageTopPanel;
+    private JLabel jLabel4;
+    private JLabel jLabel5;
+    private JPanel attachmentsPanel;
+    private JButton cancelButton;
+    private JButton saveButton;
+    private JLabel jLabel7;
+    private JButton sendButton;
+    private JPanel buttonPanel;
+    private JLabel jLabel6;
+    private JideButton addFolderButton;
+    private JideButton addFileButton;
+    private JPanel jPanel2;
     private JPanel recipientsPanel;
     private JPanel jPanel1;
     private JTextArea messageArea;
@@ -67,17 +82,7 @@ public class ComposeMessageFrame extends javax.swing.JFrame
                 rootPanel = new JPanel();
                 rootPanel.setBorder(new EmptyBorder(5, 5,
                     5, 5));
-                TableLayout rootPanelLayout = new TableLayout(
-                    new double[][] {
-                        { TableLayout.PREFERRED,
-                            TableLayout.FILL },
-                        { TableLayout.PREFERRED,
-                            TableLayout.PREFERRED,
-                            TableLayout.PREFERRED,
-                            TableLayout.FILL,
-                            TableLayout.FILL,
-                            TableLayout.FILL,
-                            TableLayout.FILL } });
+                TableLayout rootPanelLayout = new TableLayout(new double[][] {{TableLayout.PREFERRED, TableLayout.FILL}, {TableLayout.PREFERRED, TableLayout.PREFERRED, TableLayout.PREFERRED, TableLayout.FILL, TableLayout.PREFERRED, 0.33, TableLayout.PREFERRED}});
                 rootPanelLayout.setHGap(5);
                 rootPanelLayout.setVGap(5);
                 getContentPane().add(rootPanel,
@@ -124,10 +129,94 @@ public class ComposeMessageFrame extends javax.swing.JFrame
                     {
                         recipientsPanel = new JPanel();
                         FlowLayout recipientsPanelLayout = new FlowLayout();
-                        recipientsPanelLayout.setAlignment(FlowLayout.LEFT);
-                        jPanel1.add(recipientsPanel, BorderLayout.CENTER);
-                        recipientsPanel.setLayout(recipientsPanelLayout);
+                        
+                        recipientsPanelLayout
+                            .setAlignment(FlowLayout.LEFT);
+                        recipientsPanelLayout.setHgap(10);
+                        recipientsPanelLayout.setVgap(1);
+                        jPanel1.add(recipientsPanel,
+                            BorderLayout.CENTER);
+                        recipientsPanel
+                            .setLayout(recipientsPanelLayout);
                     }
+                }
+                {
+                    jLabel4 = new JLabel();
+                    rootPanel.add(jLabel4, "0, 4");
+                    jLabel4.setText("Attachments: ");
+                }
+                {
+                    jPanel2 = new JPanel();
+                    BoxLayout jPanel2Layout = new BoxLayout(
+                        jPanel2,
+                        javax.swing.BoxLayout.X_AXIS);
+                    rootPanel.add(jPanel2, "1, 4");
+                    jPanel2.setLayout(jPanel2Layout);
+                    {
+                        jLabel5 = new JLabel();
+                        jPanel2.add(jLabel5);
+                        jLabel5
+                            .setText("You can attach up to 2GB.");
+                        jLabel5.setFont(new java.awt.Font(
+                            "Dialog", 0, 12));
+                    }
+                    {
+                        jLabel6 = new JLabel();
+                        jPanel2.add(jLabel6);
+                        jLabel6.setMaximumSize(new java.awt.Dimension(100000, 100000));
+                    }
+                    {
+                        addFileButton = new JideButton();
+                        addFileButton
+                            .setButtonStyle(JideButton.HYPERLINK_STYLE);
+                        addFileButton.setAlwaysShowHyperlink(true);
+                        jPanel2.add(addFileButton);
+                        addFileButton.setText("Add file");
+                        addFileButton.setForeground(new java.awt.Color(0,0,255));
+                        addFileButton.setFont(new java.awt.Font("Dialog",0,12));
+                    }
+                    {
+                        addFolderButton = new JideButton();
+                        jPanel2.add(addFolderButton);
+                        addFolderButton.setText("Add folder");
+                        addFolderButton.setForeground(new java.awt.Color(0,0,255));
+                        addFolderButton.setFont(new java.awt.Font("Dialog",0,12));
+                        addFolderButton.setAlwaysShowHyperlink(true);
+                        addFolderButton.setButtonStyle(3);
+                    }
+                }
+                {
+                    buttonPanel = new JPanel();
+                    BoxLayout buttonPanelLayout = new BoxLayout(buttonPanel, javax.swing.BoxLayout.X_AXIS);
+                    rootPanel.add(buttonPanel, "0, 6, 1, 6");
+                    buttonPanel.setLayout(buttonPanelLayout);
+                    {
+                        jLabel7 = new JLabel();
+                        buttonPanel.add(jLabel7);
+                        jLabel7.setMaximumSize(new java.awt.Dimension(100000, 100000));
+                    }
+                    {
+                        sendButton = new JButton();
+                        buttonPanel.add(sendButton);
+                        sendButton.setText("Send");
+                    }
+                    {
+                        saveButton = new JButton();
+                        buttonPanel.add(saveButton);
+                        saveButton.setText("Save as draft");
+                    }
+                    {
+                        cancelButton = new JButton();
+                        buttonPanel.add(cancelButton);
+                        cancelButton.setText("Cancel");
+                    }
+                }
+                {
+                    attachmentsPanel = new JPanel();
+                    BoxLayout attachmentsPanelLayout = new BoxLayout(attachmentsPanel, javax.swing.BoxLayout.Y_AXIS);
+                    rootPanel.add(attachmentsPanel, "0, 5, 1, 5");
+                    attachmentsPanel.setLayout(attachmentsPanelLayout);
+                    attachmentsPanel.setBackground(new java.awt.Color(255,255,255));
                 }
             }
             pack();
