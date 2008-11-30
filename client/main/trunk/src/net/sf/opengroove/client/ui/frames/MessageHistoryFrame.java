24353e4b540368c916f162c219012306d7b8905c
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-11-30 09:00:22 +0000

    Created the basic layout of the message history frame. All that's really left now is to add the table model and columns, and implement the action listeners and stuff.
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@1310 124d5c61-e844-0410-bd6f-bad94b02dd72

--- client/main/trunk/src/net/sf/opengroove/client/ui/frames/MessageHistoryFrame.java
+++ client/main/trunk/src/net/sf/opengroove/client/ui/frames/MessageHistoryFrame.java
@@ -1,7 +1,23 @@
 package net.sf.opengroove.client.ui.frames;
 
+import info.clearthought.layout.TableLayout;
+
+import java.awt.BorderLayout;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+
 import javax.swing.WindowConstants;
 import javax.swing.SwingUtilities;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
 
 /**
  * This code was edited or generated using CloudGarden's Jigloo SWT/Swing GUI
@@ -15,6 +31,17 @@ import javax.swing.SwingUtilities;
  */
 public class MessageHistoryFrame extends javax.swing.JFrame
 {
+    private JPanel rootPanel;
+    private JPanel lowerPanel;
+    private JPanel toolbarPanel;
+    private JTextField searchField;
+    private JLabel searchHeaderLabel;
+    private JScrollPane tableScrollPane;
+    private JTable table;
+    private JLabel toolbarSpacerLabel;
+    private JButton openButton;
+    private JPanel contentPanel;
+    private JButton okButton;
     
     /**
      * Auto-generated main method to display this JFrame
@@ -43,8 +70,90 @@ public class MessageHistoryFrame extends javax.swing.JFrame
         try
         {
             setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+            {
+                rootPanel = new JPanel();
+                BorderLayout rootPanelLayout = new BorderLayout();
+                rootPanel.setLayout(rootPanelLayout);
+                rootPanel.setBorder(new EmptyBorder(10, 10,
+                    10, 10));
+                getContentPane().add(rootPanel,
+                    BorderLayout.CENTER);
+                {
+                    lowerPanel = new JPanel();
+                    BorderLayout lowerPanelLayout = new BorderLayout();
+                    rootPanel.add(lowerPanel,
+                        BorderLayout.SOUTH);
+                    lowerPanel.setLayout(lowerPanelLayout);
+                    {
+                        okButton = new JButton();
+                        lowerPanel.add(okButton,
+                            BorderLayout.EAST);
+                        okButton.setText("OK");
+                    }
+                }
+                {
+                    contentPanel = new JPanel();
+                    TableLayout contentPanelLayout = new TableLayout(new double[][] {{TableLayout.FILL}, {TableLayout.PREFERRED, TableLayout.FILL, 5.0}});
+                    contentPanelLayout.setHGap(5);
+                    contentPanelLayout.setVGap(5);
+                    rootPanel.add(contentPanel,
+                        BorderLayout.CENTER);
+                    contentPanel
+                        .setLayout(contentPanelLayout);
+                    {
+                        toolbarPanel = new JPanel();
+                        BoxLayout toolbarPanelLayout = new BoxLayout(
+                            toolbarPanel,
+                            javax.swing.BoxLayout.X_AXIS);
+                        toolbarPanel
+                            .setLayout(toolbarPanelLayout);
+                        contentPanel.add(toolbarPanel,
+                            "0, 0");
+                        {
+                            openButton = new JButton();
+                            toolbarPanel.add(openButton);
+                            openButton.setText("Open");
+                        }
+                        {
+                            toolbarSpacerLabel = new JLabel();
+                            toolbarPanel
+                                .add(toolbarSpacerLabel);
+                            toolbarSpacerLabel
+                                .setMaximumSize(new java.awt.Dimension(
+                                    1000000, 1000000));
+                        }
+                        {
+                            searchHeaderLabel = new JLabel();
+                            toolbarPanel
+                                .add(searchHeaderLabel);
+                            searchHeaderLabel
+                                .setText("Search: ");
+                        }
+                        {
+                            searchField = new JTextField(12);
+                            searchField
+                                .setMaximumSize(searchField
+                                    .getPreferredSize());
+                            toolbarPanel.add(searchField);
+                        }
+                    }
+                    {
+                        tableScrollPane = new JScrollPane();
+                        contentPanel.add(tableScrollPane, "0, 1");
+                        {
+                            TableModel tableModel = 
+                                new DefaultTableModel(
+                                    new String[][] { { "One", "Two" }, { "Three", "Four" } },
+                                    new String[] { "Column 1", "Column 2" });
+                            table = new JTable();
+                            tableScrollPane.setViewportView(table);
+                            table.setModel(tableModel);
+                        }
+                    }
+                }
+            }
             pack();
-            this.setSize(730, 655);
+            this.setSize(687, 606);
         }
         catch (Exception e)
         {
