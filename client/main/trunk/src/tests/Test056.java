dc5b8ff3e0ca5574de2766895a622743e50394ca
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2008-12-10 06:38:42 +0000

    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@1435 124d5c61-e844-0410-bd6f-bad94b02dd72

---
+++ client/main/trunk/src/tests/Test056.java
@@ -0,0 +1,34 @@
+package tests;
+
+import java.awt.BorderLayout;
+
+import javax.swing.*;
+
+/**
+ * A class for testing out javascript inside a JEditorPane.
+ * 
+ * @author Alexander Boyd
+ * 
+ */
+public class Test056
+{
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame("test");
+        frame.setSize(400, 300);
+        frame.setLocationRelativeTo(null);
+        JEditorPane editorPane = new JEditorPane();
+        frame.getContentPane()
+            .setLayout(new BorderLayout());
+        frame.getContentPane().add(editorPane);
+        frame.show();
+        editorPane.setContentType("text/html");
+        editorPane
+            .setText("<html><head></head><body> test <b>text</b></body></html>");
+    }
+    
+}
