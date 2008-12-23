3a9a866d4120d01f1d8d3dff9ddc2de89d3cb24d
javawizard@trivergia.com <javawizard@trivergia.com@9d1251d6-d08c-11dd-bc61-0ff7aae23933>
2008-12-23 03:40:44 +0000

    Added support for getting the id of a player that just joined, and added an exmaple that demonstrates this
    git-svn-id: http://jzapi.googlecode.com/svn@6 9d1251d6-d08c-11dd-bc61-0ff7aae23933

--- trunk/src/org/bzflag/jzapi/events/BzfsPlayerJoinPartEvent.java
+++ trunk/src/org/bzflag/jzapi/events/BzfsPlayerJoinPartEvent.java
@@ -4,5 +4,7 @@ import org.bzflag.jzapi.BzfsEvent;
 
 public class BzfsPlayerJoinPartEvent extends BzfsEvent
 {
+    public native int getPlayerId();
     
+    public native void setPlayerId(int playerId);
 }
