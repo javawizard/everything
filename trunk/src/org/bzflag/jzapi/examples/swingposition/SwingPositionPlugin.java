889c473bbf47465c418594b4b83be5a16ac3eee5
javawizard@trivergia.com <javawizard@trivergia.com@9d1251d6-d08c-11dd-bc61-0ff7aae23933>
2008-12-24 07:11:10 +0000

    Working on an example plugin that draws player positions in a swing window
    git-svn-id: http://jzapi.googlecode.com/svn@23 9d1251d6-d08c-11dd-bc61-0ff7aae23933

---
+++ trunk/src/org/bzflag/jzapi/examples/swingposition/SwingPositionPlugin.java
@@ -0,0 +1,27 @@
+package org.bzflag.jzapi.examples.swingposition;
+
+import javax.swing.JFrame;
+
+/**
+ * This plugin opens a Swing window, with a square in it that shows the current
+ * location of each player. It refreshes itself once every second. Players that
+ * are paused are drawn as light-gray, and dead players are not drawn.
+ * 
+ * @author Alexander Boyd
+ * 
+ */
+public class SwingPositionPlugin
+{
+    private static JFrame frame;
+    
+    public static void load(String args)
+    {
+        
+    }
+    
+    public static void unload()
+    {
+        frame.dispose();
+    }
+    
+}
