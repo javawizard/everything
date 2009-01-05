e9efc5127462d69f753546fc901bac4a95c986c4
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2009-01-05 02:16:45 +0000

    Removed most of the Progress class, since it isn't really used and since most of the class had errors in it, and added other needed jars to OpenGroove Commomn.
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@1479 124d5c61-e844-0410-bd6f-bad94b02dd72

--- common/trunk/src/net/sf/opengroove/common/utils/Progress.java
+++ common/trunk/src/net/sf/opengroove/common/utils/Progress.java
@@ -1,13 +1,9 @@
 package net.sf.opengroove.common.utils;
 
-import net.sf.opengroove.client.com.ListenerManager;
-import net.sf.opengroove.client.com.Notifier;
 
 public class Progress
 {
     private double value = 0;
-    private ListenerManager<ProgressListener> listeners = new ListenerManager<ProgressListener>();
-    
     public void set(double value)
     {
         if (value < 0)
@@ -15,14 +11,5 @@ public class Progress
         if (value > 1)
             value = 1;
         this.value = value;
-        listeners.notify(new Notifier<ProgressListener>()
-        {
-            
-            @Override
-            public void notify(ProgressListener listener)
-            {
-                listener.progressUpdated(Progress.this);
-            }
-        });
     }
 }
