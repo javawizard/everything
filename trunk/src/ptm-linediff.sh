3c522706636b74a627dbd9d9efda70aa2c53ad34
javawizard@trivergia.com <javawizard@trivergia.com@94c0d2ea-e9b5-11dd-bce9-bf72c0239d3a>
2009-01-24 04:03:16 +0000

    git-svn-id: http://personaltimemachine.googlecode.com/svn@14 94c0d2ea-e9b5-11dd-bce9-bf72c0239d3a

--- trunk/src/ptm-linediff.sh
+++ trunk/src/ptm-linediff.sh
@@ -3,6 +3,7 @@
 # This program takes the names of two files as command-line arguments, and writes all
 # lines that are in the first but not the second to stdout.
 if [ $# -lt 2 ] ; then
-    echo ptm-andnotfile.sh needs two files as input
+    echo ptm-linediff.sh needs two files as input
     exit 2
 fi
+diff -u $1 $2 | tail -n +3 | grep ^- | sed -e "s/^-//"
