79c62bc0ddc7954e2cf7ab769cd3f9e2fabbcc53
javawizard@trivergia.com <javawizard@trivergia.com@94c0d2ea-e9b5-11dd-bce9-bf72c0239d3a>
2009-01-24 08:34:36 +0000

    git-svn-id: http://personaltimemachine.googlecode.com/svn@35 94c0d2ea-e9b5-11dd-bce9-bf72c0239d3a

--- trunk/src/ptm-commit.sh
+++ trunk/src/ptm-commit.sh
@@ -8,10 +8,18 @@
 oldrevision=`cat .ptm/revision`
 newrevision=`expr $oldrevision + 1`
 
-# Next, we'll work on building the folder lists.
+# Next, we'll work on building the folder and file changelists.
 
-ptm-listfiles.sh d > .ptm/tmp/dirs-wc
-ptm-listfiles.sh f > .ptm/tmp/files-wc
+ptm-listfiles.sh d | sort > .ptm/tmp/dirs-wc
+ptm-listfiles.sh f | sort > .ptm/tmp/files-wc
 cd .ptm/head
-ptm-listfiles.sh d > ../tmp/dirs-head
-ptm-listfiles.sh f > ../tmp/files-head
+ptm-listfiles.sh d | sort > ../tmp/dirs-head
+ptm-listfiles.sh f | sort > ../tmp/files-head
+cd ../tmp
+ptm-linediff.sh dirs-head dirs-wc | uniq > dirs-removed
+ptm-linediff.sh dirs-wc dirs-head | uniq > dirs-added
+ptm-linediff.sh files-head files-wc | uniq > files-removed
+cd ..
+
+# Now we'll start writing to the command list file. At this point, the working directory 
+# is the .ptm folder.
