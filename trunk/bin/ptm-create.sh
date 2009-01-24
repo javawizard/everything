9a81ea53297c7cede5cc202d2a8d01109438a0ab
javawizard@trivergia.com <javawizard@trivergia.com@94c0d2ea-e9b5-11dd-bce9-bf72c0239d3a>
2009-01-24 06:47:35 +0000

    git-svn-id: http://personaltimemachine.googlecode.com/svn@23 94c0d2ea-e9b5-11dd-bce9-bf72c0239d3a

--- trunk/bin/ptm-create.sh
+++ trunk/bin/ptm-create.sh
@@ -17,12 +17,24 @@ touch revinfo
 
 # create the commandlist file 
 cat > commandlist << END_FILE
-#!/bin/sh
-if [ $# -lt 1 ] ; then
-    echo commandlist called with no arguments
+#!/bin/bash
+if [ \$# -lt 2 ] ; then
+    echo commandlist called with no arguments, needs targetfolder startrev [endrev printstatus]
     exit 2
+fi
+targetfolder=\$1
+startrev=\$2
+endrev=\$2
+if [ \$# -ge 3 ] ; then
+    endrev=\$3
+fi
+printstatus=false
+if [ \$# -ge 4 ] ; then
+    printstatus=true
 fi
 END_FILE
 chmod +x commandlist
+
+echo 0 > revision
 
 cd ..
