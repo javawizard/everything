041a15e7fd3b9bff4aef42710acc9d70676676c7
javawizard@trivergia.com <javawizard@trivergia.com@94c0d2ea-e9b5-11dd-bce9-bf72c0239d3a>
2009-01-24 02:23:48 +0000

    git-svn-id: http://personaltimemachine.googlecode.com/svn@10 94c0d2ea-e9b5-11dd-bce9-bf72c0239d3a

--- trunk/bin/ptm-create.sh
+++ trunk/bin/ptm-create.sh
@@ -4,12 +4,15 @@
 # it up with all of the files and folders that ptm needs.
 
 # TODO: check to see if the folder already exists
+
 mkdir .ptm
 cd .ptm
+
 # create folders
 mkdir head
 mkdir tmp
 mkdir diffs
+
 # create the messages file
 cat > messages << END_FILE
 #!/bin/sh
@@ -19,6 +22,7 @@ if [ $# -lt 1 ] ; then
 fi
 END_FILE
 chmod +x messages
+
 # create the commandlist file 
 echo \#\!/bin/sh > commandlist
 echo if \[ \$# -lt 1 \] \; then >> commandlist
@@ -26,4 +30,5 @@ echo echo commandlist called with no arguments >> commandlist
 echo exit 2 >> commandlist
 echo fi >> commandlist
 chmod +x commandlist
+
 cd ..
