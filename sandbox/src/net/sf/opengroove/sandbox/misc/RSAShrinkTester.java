a6e91f050e2db5307b7a3541502d38ca85840815
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2009-01-27 06:59:12 +0000

    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@1622 124d5c61-e844-0410-bd6f-bad94b02dd72

--- sandbox/src/net/sf/opengroove/sandbox/misc/RSAShrinkTester.java
+++ sandbox/src/net/sf/opengroove/sandbox/misc/RSAShrinkTester.java
@@ -1,7 +1,10 @@
 package net.sf.opengroove.sandbox.misc;
 
 import java.math.BigInteger;
+import java.security.Security;
 import java.util.Arrays;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
 import net.sf.opengroove.common.security.CertificateUtils;
 import net.sf.opengroove.common.security.RSA;
@@ -15,8 +18,9 @@ public class RSAShrinkTester
      */
     public static void main(String[] args)
     {
+        Security.addProvider(new BouncyCastleProvider());
         System.out.println("generating keypair");
-        RSA rsa = new RSA(1024);
+        RSA rsa = new RSA(1536);
         System.out.println("keypair generated");
         int wereEqual = 0;
         int wereSameSize = 0;
@@ -24,11 +28,11 @@ public class RSAShrinkTester
         {
             byte[] key = CertificateUtils
                 .generateSymmetricKey().getEncoded();
-            BigInteger keyInt = new BigInteger(1, key);
-            BigInteger keyEnc = rsa.encrypt(keyInt);
-            BigInteger keyDec = rsa.decrypt(keyEnc);
-            byte[] keyDecBytes = StringUtils.exactLength(
-                keyDec.toByteArray(), 32);
+            byte[] enc = CertificateUtils.encryptRsa(key,
+                rsa.getPublicKey(), rsa.getModulus());
+            byte[] keyDecBytes = CertificateUtils
+                .decryptRsa(enc, rsa.getPrivateKey(), rsa
+                    .getModulus());
             if (!Arrays.equals(key, keyDecBytes))
                 System.out.println(keyDecBytes.length);
         }
@@ -36,5 +40,4 @@ public class RSAShrinkTester
         System.out.println("" + wereSameSize
             + " were the same size.");
     }
-    
 }
