fdb86d6aed1ed1e38809976943cd16b273e4ef36
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2009-02-27 01:42:29 +0000

    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@1628 124d5c61-e844-0410-bd6f-bad94b02dd72

---
+++ client/main/trunk/src/tests/t20/Test003.java
@@ -0,0 +1,25 @@
+package tests.t20;
+
+/**
+ * This test deletes the database at C:\opengroove-test003-db if it exists. Then
+ * it creates the database. It then inserts a blob, reads it, modifies it, and
+ * reads it again. The main purpose of the test is to see of the H2 database
+ * supports modifying blobs and reading segments of blobs, since H2's
+ * documentation is ambiguous on this point.
+ * 
+ * @author Alexander Boyd
+ * 
+ */
+public class Test003
+{
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
