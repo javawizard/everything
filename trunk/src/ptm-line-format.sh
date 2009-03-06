3eac04621f17617049c2856d7c580aea03c2c72f
javawizard@trivergia.com <javawizard@trivergia.com@94c0d2ea-e9b5-11dd-bce9-bf72c0239d3a>
2009-03-06 05:38:38 +0000

    git-svn-id: http://personaltimemachine.googlecode.com/svn@61 94c0d2ea-e9b5-11dd-bce9-bf72c0239d3a

--- trunk/src/ptm-line-format.sh
+++ trunk/src/ptm-line-format.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-# Takes one or two arguments. 
+# Takes one or two arguments, source file and dest file. Writes to the source file if
+# only one argument.
+# This script essentially prepends carriage returns with 02, line feeds with 01, and 0 
+# chars with 03.
+
+# xxd -c 1 -p encodes, xxd -c 1 -p -r decodes
+
+sourcefile=$1
+destfile=$sourcefile
+if [ $# -gt 1 ] ; then
+    destfile=$2
+fi
+
+# The actual program...
+
+cat $sourcefile | xxd -c 1 -p | awk 'BEGIN {skip = 0
