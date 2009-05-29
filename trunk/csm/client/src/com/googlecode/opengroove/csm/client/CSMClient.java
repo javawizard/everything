aa1a81b714412d4a02f022941229bcadcc37a653
javawizard@trivergia.com <javawizard@trivergia.com@aa5ab2c2-dddc-11dd-993c-4d20d8c3c59d>
2009-05-29 04:39:55 +0000

    git-svn-id: http://opengroove.googlecode.com/svn@15 aa5ab2c2-dddc-11dd-993c-4d20d8c3c59d

---
+++ trunk/csm/client/src/com/googlecode/opengroove/csm/client/CSMClient.java
@@ -0,0 +1,15 @@
+package com.googlecode.opengroove.csm.client;
+
+public class CSMClient
+{
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
