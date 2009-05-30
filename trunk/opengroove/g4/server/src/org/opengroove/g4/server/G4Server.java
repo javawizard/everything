c43a53b9b963d48da919431687d22c81915f78bd
javawizard@trivergia.com <javawizard@trivergia.com@aa5ab2c2-dddc-11dd-993c-4d20d8c3c59d>
2009-05-30 05:32:02 +0000

    Added an initial class, plus ignore list
    git-svn-id: http://opengroove.googlecode.com/svn@41 aa5ab2c2-dddc-11dd-993c-4d20d8c3c59d

---
+++ trunk/opengroove/g4/server/src/org/opengroove/g4/server/G4Server.java
@@ -0,0 +1,15 @@
+package org.opengroove.g4.server;
+
+public class G4Server
+{
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
