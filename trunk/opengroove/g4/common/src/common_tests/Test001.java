4dd8a926303460d8ab6cedb2b7240f0beaa15e96
javawizard@trivergia.com <javawizard@trivergia.com@aa5ab2c2-dddc-11dd-993c-4d20d8c3c59d>
2009-06-05 19:47:16 +0000

    git-svn-id: http://opengroove.googlecode.com/svn@69 aa5ab2c2-dddc-11dd-993c-4d20d8c3c59d

---
+++ trunk/opengroove/g4/common/src/common_tests/Test001.java
@@ -0,0 +1,14 @@
+package common_tests;
+
+public class Test001
+{
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("Hello");
+    }
+    
+}
