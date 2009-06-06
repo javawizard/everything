e02cbfb4681bebf02f8cc60355f20bd2677d96ba
javawizard@trivergia.com <javawizard@trivergia.com@aa5ab2c2-dddc-11dd-993c-4d20d8c3c59d>
2009-06-06 00:03:11 +0000

    git-svn-id: http://opengroove.googlecode.com/svn@84 aa5ab2c2-dddc-11dd-993c-4d20d8c3c59d

--- trunk/opengroove/g4/server/src/org/opengroove/g4/server/G4Server.java
+++ trunk/opengroove/g4/server/src/org/opengroove/g4/server/G4Server.java
@@ -1,15 +1,30 @@
 package org.opengroove.g4.server;
 
+import java.io.File;
+
 public class G4Server
 {
+    private static File storageFolder;
+    /**
+     * The message store folder, Within here is one folder for each username,
+     * within that is one folder for each computer, within that is one file per
+     * message which is the serialized InboundMessage packet that should be sent
+     * for the message. OutboundMessage packets to be sent to other servers will
+     * be stored in a similar folder in the future.
+     */
+    private static File messageFolder;
+    /**
+     * The auth folder. This contains one file called db
+     */
+    private static File authFolder;
     
     /**
      * @param args
      */
     public static void main(String[] args)
     {
-        // TODO Auto-generated method stub
-        
+        storageFolder = new File("storage");
+        messageFolder = new File(storageFolder, "messages");
     }
     
 }
