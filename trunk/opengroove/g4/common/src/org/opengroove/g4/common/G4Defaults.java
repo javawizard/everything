6e0a11e363850e6d9cfa2ce5bb153f9cd9f81d31
javawizard@trivergia.com <javawizard@trivergia.com@aa5ab2c2-dddc-11dd-993c-4d20d8c3c59d>
2009-06-06 01:01:06 +0000

    Added PacketSpooler from 6jet Server, and added a port constant to G4Constants .
    git-svn-id: http://opengroove.googlecode.com/svn@87 aa5ab2c2-dddc-11dd-993c-4d20d8c3c59d

---
+++ trunk/opengroove/g4/common/src/org/opengroove/g4/common/G4Defaults.java
@@ -0,0 +1,6 @@
+package org.opengroove.g4.common;
+
+public class G4Defaults
+{
+    public static final int CLIENT_SERVER_PORT = 64472;
+}
---
+++ trunk/opengroove/g4/common/src/org/opengroove/g4/common/PacketSpooler.java
@@ -0,0 +1,74 @@
+package org.opengroove.g4.common;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class PacketSpooler extends Thread
+{
+    private ObjectOutputStream out;
+    private BlockingQueue<Packet> queue;
+    private DatagramSocket datagramSocket;
+    private InetAddress targetAddress;
+    private int targetPort;
+    
+    public PacketSpooler(ObjectOutputStream out, DatagramSocket datagramSocket,
+        InetAddress targetAddress, int targetPort, int queueSize)
+    {
+        this.out = out;
+        this.queue = new LinkedBlockingQueue<Packet>(queueSize);
+        this.datagramSocket = datagramSocket;
+        this.targetAddress = targetAddress;
+        this.targetPort = targetPort;
+    }
+    
+    public synchronized boolean send(Packet packet)
+    {
+        boolean canOffer = queue.offer(packet);
+        return canOffer;
+    }
+    
+    private boolean closed = false;
+    private final Object sendLock = new Object();
+    
+    public void close() throws IOException
+    {
+        closed = true;
+        out.close();
+    }
+    
+    public void run()
+    {
+        try
+        {
+            while (true)
+            {
+                Packet packet = queue.take();
+                synchronized (sendLock)
+                {
+                    out.writeObject(packet);
+                    out.flush();
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            if (closed)
+            {
+                System.out.println("Closed packet spooler with normal close exception");
+            }
+            else
+            {
+                e.printStackTrace();
+                System.out
+                    .println("Closed packet spooler with the above abnormal exception");
+            }
+        }
+    }
+    
+}
