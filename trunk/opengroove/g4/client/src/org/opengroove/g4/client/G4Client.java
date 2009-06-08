99f81544f1a65826801f70d7d449c3dc60143844
javawizard@trivergia.com <javawizard@trivergia.com@aa5ab2c2-dddc-11dd-993c-4d20d8c3c59d>
2009-06-08 03:49:51 +0000

    ok, this really had better have worked, or I'm going to be very mad
    git-svn-id: http://opengroove.googlecode.com/svn@125 aa5ab2c2-dddc-11dd-993c-4d20d8c3c59d

--- trunk/opengroove/g4/client/src/org/opengroove/g4/client/G4Client.java
+++ trunk/opengroove/g4/client/src/org/opengroove/g4/client/G4Client.java
@@ -2,7 +2,5 @@ package org.opengroove.g4.client;
 
 public class G4Client
 {
-    /*
-     * Testing to see if I can commit this
-     */
+    
 }
--- trunk/opengroove/g4/client/src/org/opengroove/g4/client/Statics.java
+++ trunk/opengroove/g4/client/src/org/opengroove/g4/client/Statics.java
@@ -1,20 +1,23 @@
 package org.opengroove.g4.client;
 
 /**
- * Contains one method, {@link #run()}, which should be called before
- * OpenGroove starts, or before any classes that use OpenGroove start.
- * Currently, it just activates the license for the Jidesoft component
- * libraries.
+ * Contains one method, {@link #run()}, which should be called before OpenGroove
+ * starts, or before any classes that use OpenGroove start. Currently, it just
+ * activates the license for the Jidesoft component libraries.
  * 
  * @author Alexander Boyd
  * 
  */
 public class Statics
 {
-    public static void run()
+    private static boolean wasRun = false;
+    
+    public static synchronized void run()
     {
-        com.jidesoft.utils.Lm.verifyLicense(
-            "Trivergia Technologies, LLC", "OpenGroove",
-            "pfMGeSDFibz9ga5TsECdMuheO6AV0Jw2");
+        if (wasRun)
+            return;
+        wasRun = true;
+        com.jidesoft.utils.Lm.verifyLicense("Trivergia Technologies, LLC",
+            "OpenGroove", "pfMGeSDFibz9ga5TsECdMuheO6AV0Jw2");
     }
 }
