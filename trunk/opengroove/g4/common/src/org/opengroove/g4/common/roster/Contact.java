00c8fd28acd3e131ed1e19c7d1ccd308b49259c3
javawizard@trivergia.com <javawizard@trivergia.com@aa5ab2c2-dddc-11dd-993c-4d20d8c3c59d>
2009-06-10 01:20:58 +0000

    Added the "exists" field to Contact
    git-svn-id: http://opengroove.googlecode.com/svn@133 aa5ab2c2-dddc-11dd-993c-4d20d8c3c59d

--- trunk/opengroove/g4/common/src/org/opengroove/g4/common/roster/Contact.java
+++ trunk/opengroove/g4/common/src/org/opengroove/g4/common/roster/Contact.java
@@ -6,9 +6,31 @@ import org.opengroove.g4.common.user.Userid;
 
 public class Contact implements Serializable
 {
+    /**
+     * The username userid of this contact
+     */
     private Userid userid;
+    /**
+     * The local name that this user has set for the contact, or null if the
+     * user has not set a local name for this contact
+     */
     private String name;
+    /**
+     * True if this contact has been set as visible, false if this contact has
+     * been hidden. In the old G3 terminology, a hidden contact was referred to
+     * as a "known user" and a shown contact was referred to as a "contact".
+     */
     private boolean visible;
+    /**
+     * True if this contact actually exists, false if they do not. Any userid
+     * can be added to a user's contact list without problem, but the contact
+     * will come back in the roster as non-existent. This is checked whenever
+     * the roster is to be sent to the user, so if a nonexistent contact is
+     * created but then that userid is later registered, then the next time any
+     * users with that user as a contact connect they will get the new
+     * information that the user does exist.
+     */
+    private boolean exists;
     
     public Userid getUserid()
     {
@@ -38,5 +60,15 @@ public class Contact implements Serializable
     public void setVisible(boolean visible)
     {
         this.visible = visible;
+    }
+    
+    public boolean isExists()
+    {
+        return exists;
+    }
+    
+    public void setExists(boolean exists)
+    {
+        this.exists = exists;
     }
 }
