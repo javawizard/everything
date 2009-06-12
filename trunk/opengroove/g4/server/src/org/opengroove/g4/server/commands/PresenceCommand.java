e372c1d02c3d34a0d37d6bdc6647036ed606ad43
javawizard@trivergia.com <javawizard@trivergia.com@aa5ab2c2-dddc-11dd-993c-4d20d8c3c59d>
2009-06-12 01:57:19 +0000

    PresenceCommand should be done. Now all that's left is initial presence and roster sending, and sending of presence to all rosters that use a user when the user signs on or signs off.
    git-svn-id: http://opengroove.googlecode.com/svn@146 aa5ab2c2-dddc-11dd-993c-4d20d8c3c59d

---
+++ trunk/opengroove/g4/server/src/org/opengroove/g4/server/commands/PresenceCommand.java
@@ -0,0 +1,31 @@
+package org.opengroove.g4.server.commands;
+
+import org.opengroove.g4.common.protocol.PresencePacket;
+import org.opengroove.g4.common.protocol.PresencePacket.Status;
+import org.opengroove.g4.server.Command;
+import org.opengroove.g4.server.G4Server;
+import org.opengroove.g4.server.ServerConnection;
+import org.opengroove.g4.server.commands.types.ComputerCommand;
+
+@ComputerCommand
+public class PresenceCommand implements Command<PresencePacket>
+{
+    
+    public void process(PresencePacket packet)
+    {
+        /*
+         * Broadcasting offline presence packets isn't allowed for now, so if we
+         * see an offline presence packet, we'll ignore it.
+         */
+        if (packet.getStatus() == Status.Offline)
+        {
+            System.out.println("Warning: offline status packet received, ignoring");
+        }
+        ServerConnection con = ServerConnection.getConnection();
+        /*
+         * Prevent the user from spoofing the stauts of other users
+         */
+        packet.setUserid(con.userid);
+        G4Server.updateContainingPresence(con.userid, packet);
+    }
+}
