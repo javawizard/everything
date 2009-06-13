3b52ac0929fa0ffa17a9f9c890bbd4e2ea3f87da
javawizard@trivergia.com <javawizard@trivergia.com@aa5ab2c2-dddc-11dd-993c-4d20d8c3c59d>
2009-06-13 04:56:00 +0000

    Added SOCKET_TIMEOUT as a default
    git-svn-id: http://opengroove.googlecode.com/svn@153 aa5ab2c2-dddc-11dd-993c-4d20d8c3c59d

--- trunk/opengroove/g4/common/src/org/opengroove/g4/common/G4Defaults.java
+++ trunk/opengroove/g4/common/src/org/opengroove/g4/common/G4Defaults.java
@@ -3,5 +3,6 @@ package org.opengroove.g4.common;
 public class G4Defaults
 {
     public static final int CLIENT_SERVER_PORT = 64472;
-    public static final int NOP_INTERVAL = 30 * 1000;
+    public static final int NOP_INTERVAL = 25 * 1000;
+    public static final int SOCKET_TIMEOUT = 40 * 1000;
 }
