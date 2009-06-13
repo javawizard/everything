22c3a05dff13d1715cde17f8264d04702be9dc9b
javawizard@trivergia.com <javawizard@trivergia.com@aa5ab2c2-dddc-11dd-993c-4d20d8c3c59d>
2009-06-13 04:55:34 +0000

    Added NOP_INTERVAL as a default.
    git-svn-id: http://opengroove.googlecode.com/svn@152 aa5ab2c2-dddc-11dd-993c-4d20d8c3c59d

--- trunk/opengroove/g4/common/src/org/opengroove/g4/common/G4Defaults.java
+++ trunk/opengroove/g4/common/src/org/opengroove/g4/common/G4Defaults.java
@@ -2,5 +2,6 @@ package org.opengroove.g4.common;
 
 public class G4Defaults
 {
-    public static final int CLIENT_SERVER_PORT = 64472; 
+    public static final int CLIENT_SERVER_PORT = 64472;
+    public static final int NOP_INTERVAL = 30 * 1000;
 }
