0ae7b5b4124b479be744bfa5a0269ec6fc77252e
javawizard2539 <javawizard2539@124d5c61-e844-0410-bd6f-bad94b02dd72>
2009-06-14 05:17:48 +0000

    Added more exception-related classes, and a note on LinkException indicating that the exception type is not currently used
    git-svn-id: svn://svn.code.sf.net/p/opengroove/code@1643 124d5c61-e844-0410-bd6f-bad94b02dd72

---
+++ client/main/trunk/src/net/sf/opengroove/client/com/DisconnectedException.java
@@ -0,0 +1,13 @@
+package net.sf.opengroove.client.com;
+
+/**
+ * An exception that is thrown from the query() methods on Communicator when the
+ * connection to the server is lost while waiting for a response.
+ * 
+ * @author Alexander Boyd
+ * 
+ */
+public class DisconnectedException extends RuntimeException
+{
+    
+}
