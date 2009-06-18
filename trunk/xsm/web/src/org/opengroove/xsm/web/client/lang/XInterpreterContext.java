b811d2dbb19a4ccea6eb338167a9aa2db4d48945
javawizard@trivergia.com <javawizard@trivergia.com@aa5ab2c2-dddc-11dd-993c-4d20d8c3c59d>
2009-06-18 15:34:06 +0000

    Added initial values to XInterpreterContext
    git-svn-id: http://opengroove.googlecode.com/svn@204 aa5ab2c2-dddc-11dd-993c-4d20d8c3c59d

--- trunk/xsm/web/src/org/opengroove/xsm/web/client/lang/XInterpreterContext.java
+++ trunk/xsm/web/src/org/opengroove/xsm/web/client/lang/XInterpreterContext.java
@@ -1,6 +1,21 @@
 package org.opengroove.xsm.web.client.lang;
 
+import java.util.HashMap;
+
 public class XInterpreterContext
 {
-    
+    private XInterpreter interpreter;
+    private HashMap<String, XData> variables = new HashMap<String, XData>();
+    public XInterpreter getInterpreter()
+    {
+        return interpreter;
+    }
+    public void setInterpreter(XInterpreter interpreter)
+    {
+        this.interpreter = interpreter;
+    }
+    public HashMap<String, XData> getVariables()
+    {
+        return variables;
+    }
 }
