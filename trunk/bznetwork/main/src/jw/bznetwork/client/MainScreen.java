c275aec59ac9591a714425000632f27fcfb5fb6f
javawizard@trivergia.com <javawizard@trivergia.com@dcf11152-862b-11de-82e5-a1837a67dc72>
2009-07-23 05:06:34 +0000

    More changes...
    git-svn-id: http://bznetwork.googlecode.com/svn@61 dcf11152-862b-11de-82e5-a1837a67dc72

--- trunk/bznetwork/main/src/jw/bznetwork/client/MainScreen.java
+++ trunk/bznetwork/main/src/jw/bznetwork/client/MainScreen.java
@@ -1,7 +1,17 @@
 package jw.bznetwork.client;
 
+import java.util.ArrayList;
+
+import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.ClickListener;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlexTable;
+import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.PopupPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.Widget;
 
 /**
  * The main BZNetwork screen. This class is built to be independent of BZNetwork
@@ -27,18 +37,85 @@ import com.google.gwt.user.client.ui.Composite;
  */
 public class MainScreen extends Composite
 {
+    private Label headerLabel;
+    private Label headerScreenLabel;
+    private ArrayList<Screen> screens = new ArrayList<Screen>();
+    private Screen selectedScreen;
+    
     /**
      * Creates a new MainScreen.
      * 
-     * @param header The text that should be shown in the header
-     * @param headerScreenName True to show the name of the screen
+     * @param header
+     *            The text that should be shown in the header
+     * @param headerScreenName
+     *            True to show the name of the current screen right after the
+     *            header
      * @param links
+     *            The list of links, which can contain html
      * @param listeners
+     *            The listeners that correspond to the links given
      * @param screens
+     *            The screens that should be installed. There must be at least
+     *            one of these.
      */
+    @SuppressWarnings("deprecation")
     public MainScreen(String header, boolean headerScreenName, String[] links,
             ClickListener[] listeners, Screen[] screens)
     {
+        this.headerLabel = new Label(header);
+        this.headerScreenLabel = new Label("");
+        String wrapperDivId = HTMLPanel.createUniqueId();
+        HTMLPanel wrapper = new HTMLPanel(
+                "<table width='100%' border='0' cellspacing='0' cellpadding='20'><tr><td><div id='"
+                        + wrapperDivId
+                        + "' style='width:100%'></div></td></tr></table>");
+        initWidget(wrapper);
+        VerticalPanel mainPagePanel = new VerticalPanel();
+        wrapper.add(mainPagePanel, wrapperDivId);
+        mainPagePanel.setWidth("100%");
+        FlexTable topTable = new FlexTable();
+        HorizontalPanel headerPositionWidget = new HorizontalPanel();
+        headerLabel.addStyleName("bznetwork-MainScreenHeader");
+        headerPositionWidget.add(headerLabel);
+        if (headerScreenName)
+        {
+            Label headerSeparator = new Label(":");
+            headerSeparator.addStyleName("bznetwork-MainScreenHeaderSeparator");
+            headerPositionWidget.add(headerSeparator);
+            headerScreenLabel.addStyleName("bznetwork-MainScreenHeaderName");
+            headerPositionWidget.add(headerScreenLabel);
+        }
+        topTable.setWidget(0, 0, headerPositionWidget);
+        HorizontalPanel upperRightPanel = new HorizontalPanel();
+        final Anchor menuAnchor = new Anchor("Menu");
+        menuAnchor.addClickListener(new ClickListener()
+        {
+            
+            @Override
+            public void onClick(Widget sender)
+            {
+                PopupPanel box = new PopupPanel(true, false);
+                box.setPopupPosition(menuAnchor.getAbsoluteLeft(), menuAnchor
+                        .getAbsoluteTop()
+                        + menuAnchor.getOffsetHeight());
+                VerticalPanel dialogPanel = new VerticalPanel();
+                dialogPanel.add(new Anchor("Reports"));
+                dialogPanel.add(new Anchor("Bans"));
+                dialogPanel
+                        .add(new Anchor(
+                                "<span class='bznetwork-MenuCurrentScreenItem'>Servers</span>",
+                                true));
+                dialogPanel.add(new Anchor("Logs"));
+                dialogPanel.add(new Anchor("Live"));
+                dialogPanel.add(new Anchor("Users"));
+                dialogPanel.add(new Anchor("Callsigns"));
+                dialogPanel.add(new Anchor("Roles"));
+                dialogPanel.add(new Anchor("Authentication"));
+                dialogPanel.add(new Anchor("Help"));
+                box.setWidget(dialogPanel);
+                box.show();
+            }
+        });
         
     }
 }
