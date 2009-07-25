b95844da59cae19e8d9aeab37bdbcd67b7e5ca22
javawizard@trivergia.com <javawizard@trivergia.com@dcf11152-862b-11de-82e5-a1837a67dc72>
2009-07-25 08:51:47 +0000

    Some threading stuff
    git-svn-id: http://bznetwork.googlecode.com/svn@85 dcf11152-862b-11de-82e5-a1837a67dc72

--- trunk/bznetwork/plugin/bznetwork.cpp
+++ trunk/bznetwork/plugin/bznetwork.cpp
@@ -4,18 +4,74 @@
 #include "bzfsAPI.h"
 #include "plugin_utils.h"
 #include <iostream>
+#include <stdio.h>
+#include <assert.h>
+#include <pthread.h>
 
 BZ_GET_PLUGIN_VERSION
 
-BZF_PLUGIN_CALL int bz_Load(const char* /*commandLine*/) {
-	cout << "-------- Loaded the genobait plugin" << endl;
-	bz_debugMessage(4, "genobait plugin loaded");
+std::vector<std::string> stdinList;
+pthread_mutex_t stdinListLock =
+PTHREAD_MUTEX_INITIALIZER;
+pthread_t stdinReadThread;
+
+void bzn_outputData(std::string value);
+
+class BZNetworkEventHandler: public bz_EventHandler,
+		public bz_CustomSlashCommandHandler
+{
+	public:
+		virtual void process(bz_EventData *eventData)
+		{
+		}
+		virtual bool handle(int playerID, bzApiString command,
+				bzApiString message, bzAPIStringList *params)
+		{
+			if (strcasecmp(command.c_str(), "bzn")) // is it for me ?
+				return false;
+			bzn_outputData("Message received");
+			bz_sendTextMessage(BZ_SERVER, playerID,
+					"I just received a /bzn command from you.");
+			return true;
+		}
+};
+
+void* threadedStdinReadLoop(void* bogus);
+
+BZNetworkEventHandler singleEventHandler;
+
+BZF_PLUGIN_CALL int bz_Load(const char* /*commandLine*/)
+{
+	bz_registerCustomSlashCommand("bzn", &singleEventHandler);
+	// Perhaps allow this to be configured via an argument, and
+	// then have this value be a BZNetwork configuration setting
+	bz_setMaxWaitTime(1.0);
+	if (pthread_create(&stdinReadThread, NULL, &threadedStdinReadLoop, NULL))
+	{
+		bzn_outputData(
+				"bznfail readthread The stdin read thread could not be created.");
+		return 1;
+	}
+	bzn_outputData("bznload");
 	return 0;
 }
 
-BZF_PLUGIN_CALL int bz_Unload(void) {
-	bz_debugMessage(4, "genobait plugin unloaded");
+void bzn_outputData(std::string value)
+{
+	assert(value.length() < 65529);
+	printf("|%.5d%s\n", value.length(), value.c_str());
+}
+
+int bz_Unload(void)
+{
+	bz_removeCustomSlashCommand("bzn");
+	bzn_outputData("bznunload");
 	return 0;
+}
+
+void* threadedStdinReadLoop(void* bogus)
+{
+
 }
 
 // Local Variables: ***
