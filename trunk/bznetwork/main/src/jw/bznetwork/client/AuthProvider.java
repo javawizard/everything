ab00f266f309f52b209b0209fe365222e1481216
javawizard@trivergia.com <javawizard@trivergia.com@dcf11152-862b-11de-82e5-a1837a67dc72>
2009-07-28 19:49:34 +0000

    !@#$ this stupid serialization is not being picked up by GWT
    git-svn-id: http://bznetwork.googlecode.com/svn@117 dcf11152-862b-11de-82e5-a1837a67dc72

--- trunk/bznetwork/main/src/jw/bznetwork/client/AuthProvider.java
+++ trunk/bznetwork/main/src/jw/bznetwork/client/AuthProvider.java
@@ -4,6 +4,10 @@ import java.io.Serializable;
 
 public class AuthProvider implements Serializable
 {
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 3962776083585075455L;
     private String id;
     private String name;
     private String text;
