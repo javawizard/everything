d02a3af33bc49878100897782ed92e05673e7715
javawizard@trivergia.com <javawizard@trivergia.com@dcf11152-862b-11de-82e5-a1837a67dc72>
2009-08-05 14:41:53 +0000

    Added default options for the roles screen
    git-svn-id: http://bznetwork.googlecode.com/svn@250 dcf11152-862b-11de-82e5-a1837a67dc72

--- trunk/bznetwork/main/src/jw/bznetwork/client/screens/LogsScreen.java
+++ trunk/bznetwork/main/src/jw/bznetwork/client/screens/LogsScreen.java
@@ -8,43 +8,34 @@ public class LogsScreen extends VerticalScreen
     @Override
     public void deselect()
     {
-        // TODO Auto-generated method stub
-        
     }
     
     @Override
     public String getName()
     {
-        // TODO Auto-generated method stub
-        return null;
+        return "logs";
     }
     
     @Override
     public String getTitle()
     {
-        // TODO Auto-generated method stub
-        return null;
+        return "Logs";
     }
     
     @Override
     public void init()
     {
-        // TODO Auto-generated method stub
-        
     }
     
     @Override
     public void reselect()
     {
-        // TODO Auto-generated method stub
-        
+        select();
     }
     
     @Override
     public void select()
     {
-        // TODO Auto-generated method stub
-        
     }
     
 }
