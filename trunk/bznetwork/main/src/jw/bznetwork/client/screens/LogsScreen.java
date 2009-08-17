9d87dbfff733ae0dbccd379d4e68d218e02d2b19
javawizard@trivergia.com <javawizard@trivergia.com@dcf11152-862b-11de-82e5-a1837a67dc72>
2009-08-17 06:25:00 +0000

    More logs work. I'm headed to bed for the night.
    git-svn-id: http://bznetwork.googlecode.com/svn@394 dcf11152-862b-11de-82e5-a1837a67dc72

--- trunk/bznetwork/main/src/jw/bznetwork/client/screens/LogsScreen.java
+++ trunk/bznetwork/main/src/jw/bznetwork/client/screens/LogsScreen.java
@@ -1,8 +1,11 @@
 package jw.bznetwork.client.screens;
 
+import java.util.Date;
+
 import com.google.gwt.user.client.ui.Label;
 
 import jw.bznetwork.client.VerticalScreen;
+import jw.bznetwork.client.data.LogsFilterSettings;
 
 public class LogsScreen extends VerticalScreen
 {
@@ -11,17 +14,26 @@ public class LogsScreen extends VerticalScreen
             "event", "source", "target", "sourceteam", "targetteam",
             "ipaddress", "bzid", "email", "metadata", "data"
     };
+    private static final long ONE_DAY_MILLIS = 1000 * 60 * 60 * 24;
     /**
      * When the logs screen is selected, if this is true, then the filter
      * settings set in the fields will be loaded into the UI, and this will be
      * set to false. If this is false, the filter settings in the fields will be
-     * reset to their defaults and loaded into the UI.
+     * reset to their defaults and loaded into the UI. This is mainly used for
+     * other "logs" links that point to this page and that would want to
+     * configure the settings for viewing logs for that particular element.
      */
     public boolean preserveSettingsOnce = false;
+    
+    public LogsFilterSettings settings;
     
     @Override
     public void deselect()
     {
+        /*
+         * FIXME: clear the vertical panel to get rid of the log event table
+         * that might be hanging around
+         */
     }
     
     @Override
@@ -50,7 +62,56 @@ public class LogsScreen extends VerticalScreen
     @Override
     public void select()
     {
-        widget.add(new Label("The logs screen is coming soon!"));
+        if (preserveSettingsOnce)
+        {
+            preserveSettingsOnce = false;
+        }
+        else
+        {
+            settings = createDefaultSettings();
+        }
+        /*
+         * TODO: pick up here, create the filter components and load the
+         * settings into them
+         */
+    }
+    
+    private LogsFilterSettings createDefaultSettings()
+    {
+        LogsFilterSettings theSettings = new LogsFilterSettings();
+        theSettings.setStart(getStartOfDay(new Date()));
+        theSettings.setEnd(getEndOfDay(new Date()));
+        theSettings.setSearch("");
+        theSettings.setIgnoreCase(false);
+        for (String s : SEARCH_IN)
+            theSettings.getSearchIn().add(s);
+        // leave the server list empty for all servers
+        // leave the event list empty for all events
+        return theSettings;
+    }
+    
+    private Date getEndOfDay(Date date)
+    {
+        Date created = new Date();
+        created.setYear(date.getYear());
+        created.setMonth(date.getMonth());
+        created.setDate(date.getDate());
+        created.setHours(23);
+        created.setMinutes(59);
+        created.setSeconds(59);
+        return created;
+    }
+    
+    private Date getStartOfDay(Date date)
+    {
+        Date created = new Date();
+        created.setYear(date.getYear());
+        created.setMonth(date.getMonth());
+        created.setDate(date.getDate());
+        created.setHours(0);
+        created.setMinutes(0);
+        created.setSeconds(0);
+        return created;
     }
     
 }
