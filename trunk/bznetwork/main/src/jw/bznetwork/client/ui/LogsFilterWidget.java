b691cc3631f14c63f1fa02d3be3fac2587819e0e
javawizard@trivergia.com <javawizard@trivergia.com@dcf11152-862b-11de-82e5-a1837a67dc72>
2009-08-18 03:53:29 +0000

    Well, it worked, but everything was aligned to the middle of the table column. This commit aligns everything upward and fixes some other minor appearance issues.
    git-svn-id: http://bznetwork.googlecode.com/svn@399 dcf11152-862b-11de-82e5-a1837a67dc72

--- trunk/bznetwork/main/src/jw/bznetwork/client/ui/LogsFilterWidget.java
+++ trunk/bznetwork/main/src/jw/bznetwork/client/ui/LogsFilterWidget.java
@@ -53,6 +53,7 @@ public class LogsFilterWidget extends Composite
         VerticalPanel searchPanel = w100(new VerticalPanel());
         table.setWidget(0, 0, searchPanel);
         format.setWidth(0, 0, "16%");
+        format.setVerticalAlignment(0, 0, VerticalPanel.ALIGN_TOP);
         searchField = w100(new TextBox());
         searchPanel.add(new HTML("<b>Search for:</b>"));
         searchPanel.add(searchField);
@@ -62,29 +63,33 @@ public class LogsFilterWidget extends Composite
         VerticalPanel searchInPanel = w100(new VerticalPanel());
         table.setWidget(0, 1, searchInPanel);
         format.setWidth(0, 1, "16%");
+        format.setVerticalAlignment(0, 1, VerticalPanel.ALIGN_TOP);
         searchInPanel.add(new HTML("<b>Search in:</b>"));
         inField = w100(new ListBox(true));
         populateSearchIn(inField);
         searchInPanel.add(inField);
-        inField.setVisibleItemCount(10);
+        inField.setVisibleItemCount(17);
         // events group
         VerticalPanel eventsPanel = w100(new VerticalPanel());
         table.setWidget(0, 2, eventsPanel);
         format.setWidth(0, 2, "16%");
+        format.setVerticalAlignment(0, 2, VerticalPanel.ALIGN_TOP);
         eventsPanel.add(new HTML("<b>Events:</b>"));
         eventsField = w100(new ListBox(true));
         populateEvents(eventsField, model);
         eventsPanel.add(eventsField);
-        eventsField.setVisibleItemCount(10);
+        eventsField.setVisibleItemCount(17);
         // interval group
         FlexTable intervalTable = w100(new FlexTable());
         FlexCellFormatter intervalFormat = intervalTable.getFlexCellFormatter();
         table.setWidget(0, 3, intervalTable);
         format.setWidth(0, 3, "16%");
+        format.setVerticalAlignment(0, 3, VerticalPanel.ALIGN_TOP);
         intervalTable.setHTML(0, 0, "<b>Interval:</b>");
         intervalFormat.setColSpan(0, 0, 3);
         // start interval
         startSelectionButton = new DateButton(settings.getStart());
+        startSelectionButton.setWidth("100%");
         intervalTable.setWidget(1, 0, startSelectionButton);
         intervalFormat.setColSpan(1, 0, 3);
         startHourField = new TextBox();
@@ -100,6 +105,7 @@ public class LogsFilterWidget extends Composite
         populateTimeWidgets(startHourField, startMinuteField, settings
                 .getStart());
         startPeriodButton = new PeriodButton(isPm(settings.getStart()));
+        startPeriodButton.setWidth("100%");
         intervalTable.setWidget(3, 0, startPeriodButton);
         intervalFormat.setColSpan(3, 0, 3);
         // mid interval marker
@@ -109,6 +115,7 @@ public class LogsFilterWidget extends Composite
                 HorizontalPanel.ALIGN_CENTER);
         // end interval
         endSelectionButton = new DateButton(settings.getEnd());
+        endSelectionButton.setWidth("100%");
         intervalTable.setWidget(5, 0, endSelectionButton);
         intervalFormat.setColSpan(5, 0, 3);
         endHourField = new TextBox();
@@ -123,22 +130,27 @@ public class LogsFilterWidget extends Composite
         intervalTable.setWidget(6, 2, endMinuteField);
         populateTimeWidgets(endHourField, endMinuteField, settings.getEnd());
         endPeriodButton = new PeriodButton(isPm(settings.getEnd()));
+        endPeriodButton.setWidth("100%");
         intervalTable.setWidget(7, 0, endPeriodButton);
         intervalFormat.setColSpan(7, 0, 3);
         // servers group
         VerticalPanel serversPanel = w100(new VerticalPanel());
         table.setWidget(0, 4, serversPanel);
         format.setWidth(0, 4, "16%");
+        format.setVerticalAlignment(0, 4, VerticalPanel.ALIGN_TOP);
         serversPanel.add(new HTML("<b>Servers:</b>"));
         serversField = w100(new ListBox(true));
         populateServers(serversField, model);
         serversPanel.add(serversField);
-        serversField.setVisibleItemCount(10);
+        serversField.setVisibleItemCount(17);
         // search button group
         searchButton = new Button("Search");
         searchButton.addStyleName("bznetwork-LogFilterWidget-SearchButton");
+        searchButton.setWidth("100%");
+        searchButton.setHeight("100%");
         table.setWidget(0, 5, searchButton);
         format.setWidth(0, 5, "16%");
+        format.setVerticalAlignment(0, 5, VerticalPanel.ALIGN_TOP);
         // We're done!
     }
     
@@ -155,7 +167,7 @@ public class LogsFilterWidget extends Composite
      */
     public LogsFilterSettings getCurrentSettings()
     {
-       return null; 
+        return null;
     }
     
     private void populateServers(ListBox box, LogSearchModel model)
