3a241e6a004bc501d5c22939d8451415725a5b8c
javawizard@trivergia.com <javawizard@trivergia.com@dcf11152-862b-11de-82e5-a1837a67dc72>
2009-08-21 20:02:34 +0000

    Changed the urls of the help pages to point to the correct locations, and added a link for requesting a feature.
    git-svn-id: http://bznetwork.googlecode.com/svn@452 dcf11152-862b-11de-82e5-a1837a67dc72

--- trunk/bznetwork/main/src/jw/bznetwork/client/screens/HelpScreen.java
+++ trunk/bznetwork/main/src/jw/bznetwork/client/screens/HelpScreen.java
@@ -32,24 +32,34 @@ public class HelpScreen extends VerticalScreen
     @Override
     public void init()
     {
+        // http://code.google.com/p/bznetwork/issues/entry?template=Feature+
+        // Request
         widget.setWidth("100%");
         widget.setHorizontalAlignment(widget.ALIGN_CENTER);
         widget.add(new Spacer("5px", "40px"));
         HTML gettingStartedLink = new HTML(
-                "<span class='bznetwork-GettingStartedLink-out'><a href='http://code.google.com/p/bzsound/wiki/BZNetworkGettingStarted' target='_blank'>"
+                "<span class='bznetwork-GettingStartedLink-out'><a href='http://code.google.com/p/bznetwork/wiki/GettingStarted' target='_blank'>"
                         + "<span class='bznetwork-GettingStartedLink'>"
                         + "Getting Started" + "</span></a></span>");
         widget.add(gettingStartedLink);
         widget.add(new Spacer("5px", "3px"));
         HTML faqLink = new HTML(
-                "<span class='bznetwork-GettingStartedLink-out'><a href='http://code.google.com/p/bzsound/wiki/BZNetworkFAQ' target='_blank'>"
+                "<span class='bznetwork-GettingStartedLink-out'><a href='http://code.google.com/p/bznetwork/wiki/FrequentlyAskedQuestions' target='_blank'>"
                         + "<span class='bznetwork-GettingStartedLink'>"
                         + "Frequently Asked Questions" + "</span></a></span>");
         widget.add(faqLink);
+        widget.add(new Spacer("5px", "3px"));
+        widget
+                .add(new HTML(
+                        "<span class='bznetwork-GettingStartedLink-out'><a href='http://code.google.com/p/bznetwork/issues/entry?template=Feature+Request' target='_blank'>"
+                                + "<span class='bznetwork-GettingStartedLink'>"
+                                + "Request a Feature *" + "</span></a></span>"));
         widget.add(new Spacer("5px", "8px"));
         widget
                 .add(new HTML(
-                        "More help pages can be found <a href='http://code.google.com/p/bzsound/w/list?can=2&q=label%3Abznetwork' target='_blank'>here</a>."));
+                        "More help pages can be found <a href='http://code.google.com/p/bznetwork/w/list?can=2&q=label%3Ahelp' target='_blank'>here</a>."));
+        widget.add(new Spacer("5px", "2px"));
+        widget.add(new HTML("* A Google account is required for these links."));
     }
     
     @Override
