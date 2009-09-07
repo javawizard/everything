16ab726d1308f018b704f725761183041947c906
javawizard@trivergia.com <javawizard@trivergia.com@dcf11152-862b-11de-82e5-a1837a67dc72>
2009-09-07 07:14:54 +0000

    Checking in some format stuff I'm working on since I'm about to delete it
    git-svn-id: http://bznetwork.googlecode.com/svn@558 dcf11152-862b-11de-82e5-a1837a67dc72

---
+++ trunk/bznetwork/main/src/jw/bznetwork/client/jc/MissingFormatArgumentException.java
@@ -0,0 +1,29 @@
+package jw.bznetwork.client.jc;
+
+public class MissingFormatArgumentException extends RuntimeException
+{
+    
+    public MissingFormatArgumentException()
+    {
+        // TODO Auto-generated constructor stub
+    }
+    
+    public MissingFormatArgumentException(String message)
+    {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+    
+    public MissingFormatArgumentException(Throwable cause)
+    {
+        super(cause);
+        // TODO Auto-generated constructor stub
+    }
+    
+    public MissingFormatArgumentException(String message, Throwable cause)
+    {
+        super(message, cause);
+        // TODO Auto-generated constructor stub
+    }
+    
+}
---
+++ trunk/bznetwork/main/src/jw/bznetwork/client/jc/UnknownFormatConversionException.java
@@ -0,0 +1,29 @@
+package jw.bznetwork.client.jc;
+
+public class UnknownFormatConversionException extends RuntimeException
+{
+    
+    public UnknownFormatConversionException()
+    {
+        // TODO Auto-generated constructor stub
+    }
+    
+    public UnknownFormatConversionException(String message)
+    {
+        super(message);
+        // TODO Auto-generated constructor stub
+    }
+    
+    public UnknownFormatConversionException(Throwable cause)
+    {
+        super(cause);
+        // TODO Auto-generated constructor stub
+    }
+    
+    public UnknownFormatConversionException(String message, Throwable cause)
+    {
+        super(message, cause);
+        // TODO Auto-generated constructor stub
+    }
+    
+}
