d3ab23aaae0f42a8d56611825b9521dcf883394c
javawizard@trivergia.com <javawizard@trivergia.com@f8040f14-738c-e665-0c09-bfcd564d8029>
2010-04-11 04:08:32 +0000

    Started some actual work, and working on a class to see how stuff works thus far
    git-svn-id: http://boardforj.googlecode.com/svn@5 f8040f14-738c-e665-0c09-bfcd564d8029

--- trunk/boardforj/src/jw/boardforj/TheBoard.java
+++ trunk/boardforj/src/jw/boardforj/TheBoard.java
@@ -1,5 +1,16 @@
 package jw.boardforj;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.Date;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 /**
  * The entry point into the BoardForJ API. An instance of this class represents access to
  * the 100-hour board.
@@ -10,6 +21,7 @@ package jw.boardforj;
 public class TheBoard
 {
     public static final String DEFAULT_URL = "http://theboard.byu.edu/";
+    private static final String ACCESS_URL = "content.php?area=posts";
     private String baseUrl;
     
     /**
@@ -37,4 +49,70 @@ public class TheBoard
             url = url + "/";
         this.baseUrl = url;
     }
+    
+    /**
+     * Returns the most recent date that the board has posts for.
+     * 
+     * @return
+     */
+    public NormalDate getToday()
+    {
+        JSONObject object = query("today=1");
+        NormalDate date = new NormalDate();
+        date.setNormalYear(object.getInt("year"));
+        date.setNormalMonth(object.getInt("month"));
+        date.setDate(object.getInt("date"));
+        return date;
+    }
+    
+    /**
+     * Returns a list of all years that have posts.
+     * 
+     * @return
+     */
+    public NormalDate[] getYears()
+    {
+        JSONArray list = query("findDates=1").getJSONArray("years");
+        NormalDate[] dates = new NormalDate[list.length()];
+        for (int i = 0; i < dates.length; i++)
+        {
+            NormalDate date = new NormalDate();
+            date.setNormalYear(list.getInt(i));
+            dates[i] = date;
+        }
+        return dates;
+    }
+    
+    private JSONObject query(String params)
+    {
+        String urlString = baseUrl + ACCESS_URL;
+        if (params != null && !params.equals(""))
+            urlString += "&" + params;
+        try
+        {
+            URL url = new URL(urlString);
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            URLConnection con = url.openConnection();
+            con.addRequestProperty("Referer", "http://jzbot.opengroove.org/");
+            InputStream in = con.getInputStream();
+            byte[] buffer = new byte[256];
+            int amount;
+            while ((amount = in.read(buffer)) != -1)
+                baos.write(buffer, 0, amount);
+            in.close();
+            JSONObject object = new JSONObject(new String(baos.toByteArray()));
+            return object;
+        }
+        catch (Exception e)// IOException and JSONException, and I don't really care if it
+        // catches everything else, things will still work
+        {
+            throw new RuntimeException("Exception occurred while calling " + urlString, e);
+        }
+    }
+    
+    private String encode(String text)
+    {
+        return URLEncoder.encode(text);
+    }
+    
 }
---
+++ trunk/boardforj/src/jw/boardforj/tests/Test01.java
@@ -0,0 +1,15 @@
+package jw.boardforj.tests;
+
+public class Test01
+{
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
