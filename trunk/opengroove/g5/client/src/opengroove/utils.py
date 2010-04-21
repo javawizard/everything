e6004e1f9f257bb0ce171359f4247c9f4e707895
javawizard@trivergia.com <javawizard@trivergia.com@aa5ab2c2-dddc-11dd-993c-4d20d8c3c59d>
2010-04-21 21:06:54 +0000

    <sarcasm>Thank you google.</sarcasm> Turns out the standard library already has currying.
    git-svn-id: http://opengroove.googlecode.com/svn@380 aa5ab2c2-dddc-11dd-993c-4d20d8c3c59d

--- trunk/opengroove/g5/client/src/opengroove/utils.py
+++ trunk/opengroove/g5/client/src/opengroove/utils.py
@@ -1,30 +1,5 @@
 from traceback import print_exception
 
-# Copied from
-# http://code.activestate.com/recipes/52549-curry-associating-parameters-with-a-function/
-class Curry(object):
-    """
-A class that can wrap another function and predetermine some arguments.
-
-This class wraps any given function with a list of positional arguments and keyword 
-arguments. When the instance of this class is called, the specified function is called,
-with the predetermined positional arguments followed by those passed into the invocation
-of the instance of this class, and all of the keyword arguments together.
-""" 
-    def __init__(self, function, *args, **kwargs):
-        self.function = function
-        self.pending = args[:]
-        self.kwargs = kwargs.copy()
-
-    def __call__(self, *args, **kwargs):
-        if kwargs and self.kwargs:
-            kw = self.kwargs.copy()
-            kw.update(kwargs)
-        else:
-            kw = kwargs or self.kwargs
-
-        return self.function(*(self.pending + args), **kw)
-
 
 class BlankObject(object):
     """
