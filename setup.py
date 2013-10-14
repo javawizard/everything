727f9db251b8e6bc2e0c6a1adbc4dee49f002fde
Alexander Boyd <alex@opengroove.org>
2013-10-14 00:48:29 -0600

    Initial version

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="stm-console",
+    version="0.1.1",
+    description="A transactional variant of remote-stm",
+    author="Alexander Boyd",
+    author_email="alex@opengroove.org",
+    py_modules=["stm_console"]
+)
