7e32c189e9ce15bf994d3adac34348dd2f8b55cd
Alexander Boyd <alex@opengroove.org>
2013-11-01 02:05:47 -0600

    Initial prototype of quickdoc_generator that I've put together over the last few days.
    Note that module-level functions aren't shown yet.

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+setup(
+    name="quickdoc-generator",
+    version="0.1",
+    description="TBD",
+    author="Alexander Boyd",
+    author_email="alex@opengroove.org",
+    py_modules=["quickdoc_generator"],
+    install_requires=["fileutils", "singledispatch"]
+)
