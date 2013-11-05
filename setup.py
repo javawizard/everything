fd92b675f452c0602710408898fb41714df8c6c7
Alexander Boyd <alex@opengroove.org>
2013-11-05 02:43:08 -0700

    New version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="quickdoc",
-    version="0.1.1",
+    version="0.1.2",
     description="TBD",
     author="Alexander Boyd",
     author_email="alex@opengroove.org",
